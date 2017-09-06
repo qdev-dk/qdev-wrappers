9fcbc0a20e46528d01f2d56ba5f343a54981b3db
nataliejpg <npearson@phys.ethz.ch>
2017-09-06 15:26:16 +0200

    add wrappers files from qcodes

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='qcodes-wrappers',
+    version='0.1',
+    description='wrappers for helping to run an experiment with QCoDeS',
+    url='https://github.com/qdev-dk/qdev-wrappers',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.5',
+        'Licence :: MIT Licence',
+        'Topic :: Scientific/Engineering'
+    ],
+    license='MIT',
+    packages=['wrappers'],
+    package_data={'qcodes': ['monitor/dist/*', 'monitor/dist/js/*',
+                             'monitor/dist/css/*', 'config/*.json']},
+    install_requires=[
+        'matplotlib>=2.0.2',
+        'pyqtgraph>=0.10.0',
+        'qcodes>=0.1.3',
+        'websockets>=3.2'
+    ],
+    python_requires='>=3'
+)
---
+++ wrappers/__init__.py
@@ -0,0 +1,5 @@
+from wrappers.file_setup import my_init
+from wrappers.device_image import save_device_image
+from wrappers.show_num import show_num
+from wrappers.sweep_functions import do1d, do2d, do1dDiagonal
+from wrappers.monitor.monitor import Monitor
