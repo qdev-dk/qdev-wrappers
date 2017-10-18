4a95678fe18d7fe9f6341863836620df5aaf99fb
nataliejpg <npearson@phys.ethz.ch>
2017-10-18 23:29:41 +0200

    move device annotator to subfolder

--- wrappers/file_setup.py
+++ wrappers/file_setup.py
@@ -1,12 +1,13 @@
 from IPython import get_ipython
 import atexit
+import os
 from os.path import sep
-from os.path import abspath
-from os import makedirs
 import logging
 import qcodes as qc
+import sys
 
-from wrappers.qcodes_device_annotator import DeviceImage
+from wrappers.device_annotator.qcodes_device_annotator import DeviceImage
+from wrappers.configreader import Config
 
 log = logging.getLogger(__name__)
 CURRENT_EXPERIMENT = {}
@@ -24,14 +25,13 @@ def close_station(station):
             pass
 
 
-def _set_up_exp_file(sample_name: str, mainfolder: str= None):
+def _set_up_exp_folder(sample_name: str, mainfolder: str= None):
     """
-    Makes
     Args:
         mainfolder:  base location for the data
         sample_name:  name of the sample
     """
-    if sep in sample_name:
+    if os.path.sep in sample_name:
         raise TypeError("Use Relative names. That is without {}".format(sep))
 
     if mainfolder is None:
@@ -45,7 +45,7 @@ def _set_up_exp_file(sample_name: str, mainfolder: str= None):
     # always remove trailing sep in the main folder
     if mainfolder[-1] == sep:
         mainfolder = mainfolder[:-1]
-    mainfolder = abspath(mainfolder)
+    mainfolder = os.path.abspath(mainfolder)
 
     CURRENT_EXPERIMENT["mainfolder"] = mainfolder
     CURRENT_EXPERIMENT["sample_name"] = sample_name
@@ -53,7 +53,7 @@ def _set_up_exp_file(sample_name: str, mainfolder: str= None):
     path_to_experiment_folder = sep.join([mainfolder, sample_name, ""])
     CURRENT_EXPERIMENT["exp_folder"] = path_to_experiment_folder
     try:
-        makedirs(path_to_experiment_folder)
+        os.makedirs(path_to_experiment_folder)
     except FileExistsError:
         pass
 
@@ -67,6 +67,7 @@ def _set_up_exp_file(sample_name: str, mainfolder: str= None):
 
 def _set_up_station(station):
     CURRENT_EXPERIMENT['station'] = station
+    log.info('station set up')
 
 
 def _set_up_subfolder(subfolder_name: str):
@@ -74,7 +75,7 @@ def _set_up_subfolder(subfolder_name: str):
     sample_name = CURRENT_EXPERIMENT["sample_name"]
     CURRENT_EXPERIMENT[subfolder_name + '_subfolder'] = subfolder_name
     try:
-        makedirs(sep.join([mainfolder, sample_name, subfolder_name]))
+        os.makedirs(sep.join([mainfolder, sample_name, subfolder_name]))
     except FileExistsError:
         pass
     log.info("{} subfolder set up".format(subfolder_name))
@@ -88,6 +89,7 @@ def _init_device_image(station):
     if not success:
         di.annotateImage()
     CURRENT_EXPERIMENT['device_image'] = di
+    log.info('device image initialised')
 
 
 def _set_up_ipython_logging():
@@ -116,13 +118,74 @@ def _set_up_pdf_preferences(subfolder_name: str = 'pdf', display_pdf=True,
     pdfdisplay['combined'] = display_pdf
 
 
+def _set_up_config_file(cfg_name: str):
+    local_cfg_file = sep.join([
+        CURRENT_EXPERIMENT["mainfolder"],
+        CURRENT_EXPERIMENT["sample_name"],
+        CURRENT_EXPERIMENT['local_scripts_subfolder'],
+        "{}.config".format(cfg_name)])
+    if os.path.isfile(local_cfg_file):
+        CURRENT_EXPERIMENT["{}_config".format(cfg_name)] = 'local'
+        log.info('set up config file at {}'.format(local_cfg_file))
+    else:
+        log.warning('no config file found at {}, will try general'
+                    ''.format(local_cfg_file))
+        try:
+            script_folder = CURRENT_EXPERIMENT["scriptfolder"]
+        except KeyError:
+            raise KeyError('scriptfolder not found in CURRENT_EXPERIMENT, '
+                           'check that init function calls '
+                           '_set_up_script_folder before _set_up_config_file')
+        general_cfg_file = sep.join([script_folder,
+                                     "{}.config".format(cfg_name)])
+        if os.path.isfile(general_cfg_file):
+            CURRENT_EXPERIMENT["{}_config".format(cfg_name)] = 'general'
+            log.info('set up config file at {}'.format(general_cfg_file))
+        else:
+            log.warning('no config file found at {}'
+                        ''.format(general_cfg_file))
+
+
+def _set_up_script_folder(scriptfolder: str=None):
+    """
+    Makes
+    Args:
+        mainfolder:  base location for the data
+        sample_name:  name of the sample
+    """
+
+    if scriptfolder is None:
+        try:
+            scriptfolder = qc.config.user.scriptfolder
+        except KeyError:
+            raise KeyError('scriptfolder not set in qc.config, see '
+                           '"https://github.com/QCoDeS/Qcodes/blob/master'
+                           '/docs/examples/Configuring_QCoDeS.ipynb"')
+
+    # always remove trailing sep in the main folder
+    if scriptfolder[-1] == sep:
+        scriptfolder = scriptfolder[:-1]
+    scriptfolder = os.path.abspath(scriptfolder)
+
+    if not os.path.isdir(scriptfolder):
+        raise RuntimeError('{} is not a folder, make folder or update '
+                           'config settings at qc.config.user.scriptfolder'
+                           '"https://github.com/QCoDeS/Qcodes/blob/master'
+                           '/docs/examples/Configuring_QCoDeS.ipynb"'
+                           ''.format(scriptfolder))
+    CURRENT_EXPERIMENT["scriptfolder"] = scriptfolder
+    if scriptfolder not in sys.path:
+        sys.path.insert(0, scriptfolder)
+    log.info("general scripts folder set up at {}".format(scriptfolder))
+
 ########################################################################
 # Actual init functions
 ########################################################################
 
+
 def basic_init(sample_name: str, station, mainfolder: str= None):
     atexit.register(close_station, station)
-    _set_up_exp_file(sample_name, mainfolder)
+    _set_up_exp_folder(sample_name, mainfolder)
     _set_up_station(station)
     _set_up_ipython_logging()
 
@@ -138,23 +201,37 @@ def your_init(mainfolder: str, sample_name: str, station, plot_x_position=0.66,
         _init_device_image(station)
 
 
-def my_init(sample_name: str, station, pdf_folder=True, analysis_folder=True,
-            temp_dict_folder=True, waveforms_folder=True,
-            annotate_image=False, mainfolder: str= None, display_pdf=True,
-            display_individual_pdf=False, qubit_count=None,
-            plot_x_position=0.66,):
+def my_init(sample_name: str, station, qubit_count=None,
+            pdf_folder=True, analysis_folder=True,
+            calib_config=False, instr_config=True,
+            waveforms_folder=True,
+            local_scripts_folder=True,
+            mainfolder: str= None,
+            annotate_image=False,
+            display_pdf=True,
+            display_individual_pdf=False,
+            plot_x_position=0.66):
     basic_init(sample_name, station, mainfolder)
     CURRENT_EXPERIMENT['plot_x_position'] = plot_x_position
+    _set_up_script_folder()
     if pdf_folder:
         _set_up_pdf_preferences(display_pdf=display_pdf,
                                 display_individual_pdf=display_individual_pdf)
     if analysis_folder:
         _set_up_subfolder('analysis')
-    if temp_dict_folder:
-        _set_up_subfolder('temp_dict')
     if waveforms_folder:
         _set_up_subfolder('waveforms')
+    if any([local_scripts_folder, instr_config, calib_config]):
+        _set_up_subfolder('local_scripts')
+        f = '{}{}'.format(CURRENT_EXPERIMENT['exp_folder'],
+                          CURRENT_EXPERIMENT['local_scripts_subfolder'])
+        if f not in sys.path:
+            sys.path.insert(0, f)
+    if instr_config:
+        _set_up_config_file('instr')
+    if calib_config:
+        _set_up_config_file('calib')
     if annotate_image:
         _init_device_image(station)
     if qubit_count is not None:
-        CURRENT_EXPERIMENT["qubit_count"] = qubit_count
+        CURRENT_EXPERIMENT['qubit_count'] = qubit_count
