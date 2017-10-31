ad1b7e7d24fe0de723e8e0a32b2fc6b46b088291
Jens Hedegaard Nielsen <j.h.nielsen@nbi.ku.dk>
2017-10-31 13:52:09 +0100

    Does no longer depend on websockets

--- setup.py
+++ setup.py
@@ -20,8 +20,7 @@ setup(
     install_requires=[
         'matplotlib>=2.0.2',
         'pyqtgraph>=0.10.0',
-        'qcodes>=0.1.3',
-        'websockets>=3.2'
+        'qcodes>=0.1.3'
     ],
     python_requires='>=3'
 )
