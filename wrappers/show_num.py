490890962c0c48e953bb4f76eea5ba5542084ed6
Jens Hedegaard Nielsen <j.h.nielsen@nbi.ku.dk>
2017-10-31 16:15:07 +0100

    add wrappers

--- wrappers/show_num.py
+++ wrappers/show_num.py
@@ -4,14 +4,16 @@ from qcodes.plots.pyqtgraph import QtPlot
 from qcodes.plots.qcmatplotlib import MatPlot
 
 
-def show_num(id, useQT=False, do_plots=True):
+def show_num(id, useQT=False, do_plots=True, **kwargs):
     """
     Show  and return plot and data for id in current instrument.
     Args:
         id(number): id of instrument
         do_plots: Default False: if false no plots are produced.
+        **kwargs: Are passed to plot function
+
     Returns:
-        plot, data : returns the plot and the dataset
+        data, plots : returns the plot and the dataset
     """
     if not getattr(CURRENT_EXPERIMENT, "init", True):
         raise RuntimeError("Experiment not initalized. "
@@ -29,13 +31,14 @@ def show_num(id, useQT=False, do_plots=True):
                 if useQT:
                     plot = QtPlot(
                         getattr(data, value),
-                        fig_x_position=CURRENT_EXPERIMENT['plot_x_position'])
+                        fig_x_position=CURRENT_EXPERIMENT['plot_x_position'],
+                        ** kwargs)
                     title = "{} #{}".format(CURRENT_EXPERIMENT["sample_name"],
                                             str_id)
                     plot.subplots[0].setTitle(title)
                     plot.subplots[0].showGrid(True, True)
                 else:
-                    plot = MatPlot(getattr(data, value))
+                    plot = MatPlot(getattr(data, value), **kwargs)
                     title = "{} #{}".format(CURRENT_EXPERIMENT["sample_name"],
                                             str_id)
                     plot.subplots[0].set_title(title)
