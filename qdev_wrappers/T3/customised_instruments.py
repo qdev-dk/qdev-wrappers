a037fa9e9ef32b64405fe9641cb2cd3b84bf4b6d
Dominik Vogel <a-dovoge@microsoft.com>
2017-11-08 10:02:55 +0100

    moved fine_voltage into channel

--- qdev_wrappers/T3/customised_instruments.py
+++ qdev_wrappers/T3/customised_instruments.py
@@ -205,11 +205,58 @@ class QDAC_T10(QDac):
                                         float(config.get('Gain Settings',
                                                          'dc factor topo')))
 
+class DacChannel_T3(DacChannel):
+    """
+    A Decadac Channel with a fine_volt parameter
+    This alternative channel representation is chosen by setting the class
+    variable DAC_CHANNEL_CLASS in the main Instrument
+    """
+     def __init__(self, *args, **kwargs):
+         super().__init__(*args, **kwargs)
+         self.add_parameter("fine_volt",
+                            get_cmd=self._get_fine_voltage,
+                            set_cmd=self._set_fine_voltage,
+                            label="Voltage", unit="V"
+         )
+
+    def _get_fine_voltage(self):
+        slot = self._parent
+        if slot.slot_mode.get_latest() not in ['Fine', 'FineCald']:
+            raise RuntimeError("Cannot get fine voltage unless slot in Fine mode")
+        if self._channel == 0:
+            fine_chan = 2
+        elif self._channel == 1:
+            fine_chan = 3
+        else:
+            raise RuntimeError("Fine mode only works for Chan 0 and 1")
+        return self.volt.get() + (slot.channels[fine_chan].volt.get()+10)/200
+
+    def _set_fine_voltage(self, voltage):
+        slot = self._parent
+        if slot.slot_mode.get_latest() not in ['Fine', 'FineCald']:
+            raise RuntimeError("Cannot get fine voltage unless slot in Fine mode")
+        if self._channel == 0:
+            fine_chan = 2
+        elif self._channel == 1:
+            fine_chan = 3
+        else:
+            raise RuntimeError("Fine mode only works for Chan 0 and 1")
+        coarse_part = self._dac_code_to_v(self._dac_v_to_code(voltage-0.001))
+
+        fine_part = voltage - coarse_part
+        fine_scaled = fine_part*200-10
+        print("trying to set to {}, by setting coarse {} and fine {} with total {}".format(voltage,
+              coarse_part, fine_scaled, coarse_part+fine_part))
+        self.volt.set(coarse_part)
+        slot.channels[fine_chan].volt.set(fine_scaled)
+
 
 class Decadac_T3(Decadac):
     """
     A Decadac with one voltage dividers
     """
+    DAC_CHANNEL_CLASS = DacChannel_T3
+    SLOT_MODE_DEFAULT = "Fine"
 
     def __init__(self, name, address, config, **kwargs):
         self.config = config
@@ -217,16 +264,9 @@ class Decadac_T3(Decadac):
         deca_physical_max = int(self.config.get('Decadac Range', 'max_volt'))
         kwargs.update({'min_val': deca_physical_min,
                        'max_val': deca_physical_max})
-        # this is maybe not the prettiest solution:
-        self.SLOT_MODE_DEFAULT = "Fine"
+
         super().__init__(name, address, **kwargs)
 
-        # addtional parameters go here
-        self.add_parameter("fine_volt",
-                           get_cmd=self._get_fine_voltage,
-                           set_cmd=self._set_fine_voltage,
-                           label="Voltage", unit="V"
-                           )
         # Define the named channels
 
         # Assign labels:
@@ -300,37 +340,6 @@ class Decadac_T3(Decadac):
             self.channels[chan].volt.set_step(step)
             self.channels[chan].volt.set_delay(delay)
 
-    def _get_fine_voltage(self):
-        slot = self._parent
-        if slot.slot_mode.get_latest() not in ['Fine', 'FineCald']:
-            raise RuntimeError("Cannot get fine voltage unless slot in Fine mode")
-        if self._channel == 0:
-            fine_chan = 2
-        elif self._channel == 1:
-            fine_chan = 3
-        else:
-            raise RuntimeError("Fine mode only works for Chan 0 and 1")
-        return self.volt.get() + (slot.channels[fine_chan].volt.get()+10)/200
-
-    def _set_fine_voltage(self, voltage):
-        slot = self._parent
-        if slot.slot_mode.get_latest() not in ['Fine', 'FineCald']:
-            raise RuntimeError("Cannot get fine voltage unless slot in Fine mode")
-        if self._channel == 0:
-            fine_chan = 2
-        elif self._channel == 1:
-            fine_chan = 3
-        else:
-            raise RuntimeError("Fine mode only works for Chan 0 and 1")
-        coarse_part = self._dac_code_to_v(
-            self._dac_v_to_code(round(voltage,2)-0.01) )
-
-        fine_part = voltage - coarse_part
-        fine_scaled = fine_part*200-10
-        print("trying to set to {}, by setting coarse {} and fine {} with total {}".format(voltage,
-              coarse_part, fine_scaled, coarse_part+fine_part))
-        self.volt.set(coarse_part)
-        slot.channels[fine_chan].volt.set(fine_scaled)
 
     def set_all(self, voltage_value, set_dcbias=False):
         channels_in_use = self.config.get('Decadac Channel Labels').keys()
