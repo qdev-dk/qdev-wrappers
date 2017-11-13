2c622f44336f82af6dc86e1e30388a15a001de39
Thorvald Larsen <thorvaldw@hotmail.com>
2017-11-13 18:56:31 +0100

    show_num and show_meta
    New functionality to show_num:
    - Allows subtracting an average from each row or from each collumn
    - Can save matplotlib png files with such an average or collumn
    
    show_meta:
    New function that prints metadata to command line from a specified dataset and instruments. Also allows defining key_word to only print metadata that has key_word in label (allows printing a specific metadata value).

--- qdev_wrappers/show_num.py
+++ qdev_wrappers/show_num.py
@@ -4,18 +4,21 @@ from qcodes.plots.pyqtgraph import QtPlot
 from qcodes.plots.qcmatplotlib import MatPlot
 
 
-def show_num(id, useQT=False, do_plots=True, **kwargs):
+def show_num(id, useQT=False, ave_col=False, ave_row=False, do_plot=True, savepng=False):
     """
     Show  and return plot and data for id in current instrument.
     Args:
         id(number): id of instrument
-        do_plots: Default False: if false no plots are produced.
-        **kwargs: Are passed to plot function
+        useQT (boolean): If true plots with QTplot instead of matplotlib
+        ave_col (boolean): If true subtracts average from each collumn
+        ave_row (boolean): If true subtracts average from each row
+        savepng (boolean): If true saves matplotlib figure as png
 
     Returns:
-        data, plots : returns the plot and the dataset
+        plot, data : returns the plot and the dataset
+
     """
-    if not getattr(CURRENT_EXPERIMENT, "init", True):
+    if not CURRENT_EXPERIMENT["init"]:
         raise RuntimeError("Experiment not initalized. "
                            "use qc.Init(mainfolder, samplename)")
 
@@ -23,27 +26,88 @@ def show_num(id, useQT=False, do_plots=True, **kwargs):
 
     t = qc.DataSet.location_provider.fmt.format(counter=str_id)
     data = qc.load_data(t)
+    keys = [key for key in data.arrays.keys() if "_set" not in key[-4:]]
 
-    if do_plots:
+    if do_plot:
         plots = []
-        for value in data.arrays.keys():
-            if "set" not in value:
-                if useQT:
-                    plot = QtPlot(
-                        getattr(data, value),
-                        fig_x_position=CURRENT_EXPERIMENT['plot_x_position'],
-                        ** kwargs)
-                    title = "{} #{}".format(CURRENT_EXPERIMENT["sample_name"],
-                                            str_id)
-                    plot.subplots[0].setTitle(title)
-                    plot.subplots[0].showGrid(True, True)
-                else:
-                    plot = MatPlot(getattr(data, value), **kwargs)
-                    title = "{} #{}".format(CURRENT_EXPERIMENT["sample_name"],
-                                            str_id)
-                    plot.subplots[0].set_title(title)
-                    plot.subplots[0].grid()
-                plots.append(plot)
+        avestr = ''
+        num = ''
+        l = len(keys)
+
+        for j, value in enumerate(keys):
+            arrays = getattr(data, value)
+            if ave_row:
+                for i in range(np.shape(arrays)[0]):
+                    arrays[i,:] -= arrays[i,:].mean()
+                avestr = '_row'
+            if ave_col:
+                for i in range(np.shape(arrays)[1]):
+                    arrays[:,i] -= arrays[:,i].mean()
+                avestr = '_col'
+            if useQT:
+                plot = QtPlot(
+                    getattr(data, value),
+                    fig_x_position=CURRENT_EXPERIMENT['plot_x_position'])
+                title = "{} #{}".format(CURRENT_EXPERIMENT["sample_name"],
+                                        str_id)
+                plot.subplots[0].setTitle(title)
+                plot.subplots[0].showGrid(True, True)
+                if savepng:
+                    print('Save plot only working for matplotlib figure. Set useQT=False to save png.')
+            else:
+                plot = MatPlot(getattr(data, value))
+                if savepng:
+                    title_list_png = plot.get_default_title().split(sep)
+                    title_list_png.insert(-1, CURRENT_EXPERIMENT['png_subfolder'])
+                    title_png = sep.join(title_list_png)
+                    if l>1:
+                        num = '_{}'.format(j+1)
+                    plt.savefig("{}{}{}.png".format(title_png,num,avestr),dpi=500)
+            plots.append(plot)
     else:
         plots = None
     return data, plots
+
+
+def show_meta(id,instruments,key_word=''):
+    '''
+    Print meta data in command line for datafile id.
+    id (int): 
+    instruments (list): List of strings with instruments names for which metadata is printed.
+    key_word (string): Optional - String in label of metadata if printed.
+    '''
+
+    if not CURRENT_EXPERIMENT["init"]:
+        raise RuntimeError("Experiment not initalized. "
+                           "use qc.Init(mainfolder, samplename)")
+
+    str_id = '{0:03d}'.format(id)
+
+    t = qc.DataSet.location_provider.fmt.format(counter=str_id)
+    data = qc.load_data(t)
+    for instr in instruments:
+        new_dic = ParaPrint([data.metadata['station']['instruments'][instr]],key_word)
+        for i in range(15):
+            new_dic = ParaPrint(new_dic,key_word)
+
+
+
+
+def ParaPrint(dic_list,key_word):
+    '''
+    Helper function for show_meta. Division value is assumed added as an extra metadata atribute to parameters using 'VoltageDivider' function.
+    '''
+    new_dic_list = []
+    for dictionary in dic_list:
+        for name in dictionary.keys():
+            if (name == 'parameters') and not (not dictionary['parameters']):
+                for p_name in dictionary['parameters'].keys():
+                    p = dictionary['parameters'][p_name]
+                    if ((not (not p['value']) and not np.shape(p['value'])) or p['value']==0) and key_word in p['label']:
+                        if 'division_value' in dictionary['parameters'][p_name].keys():
+                            print('{}: {} = {} {}. Divider = {}'.format(p['instrument_name'],p['label'],p['value'],p['unit'],p['division_value']))
+                        else:
+                            print('{}: {} = {} {}'.format(p['instrument_name'],p['label'],p['value'],p['unit']))
+            if isinstance(dictionary[name], dict):
+                new_dic_list.append(dictionary[name])
+    return new_dic_list
