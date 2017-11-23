73c33460ceef289b2d2e4afda2899d0cf5594b61
nataliejpg <npearson@phys.ethz.ch>
2017-11-23 17:27:52 +0100

    make multi_qubit_init use renames customised instuments and add all to station

--- examples/init_templates/multi_qubit_init.py
+++ examples/init_templates/multi_qubit_init.py
@@ -10,17 +10,17 @@ mpl.rcParams['figure.figsize'] = (8, 3)
 mpl.rcParams['figure.subplot.bottom'] = 0.15
 mpl.rcParams['font.size'] = 8
 
-from wrappers.file_setup import CURRENT_EXPERIMENT
-from wrappers.configreader import Config
-from wrappers.file_setup import close_station, my_init
+from qdev_wrappers.file_setup import CURRENT_EXPERIMENT
+from qdev_wrappers.configreader import Config
+from qdev_wrappers.file_setup import close_station, my_init
 from qcodes import ManualParameter
 
-from wrappers import *
-from wrappers.transmon import *
+from qdev_wrappers import *
+from qdev_wrappers.transmon import *
 if qc.config.user.scriptfolder not in sys.path:
     sys.path.insert(0, qc.config.user.scriptfolder)
-from customised_instruments import Decadac_T2, AWG5014_T2, \
-    ATS9360Controller_T2, AlazarTech_ATS9360_T2, VNA_T2
+from customised_instruments import Decadac_cQED, AWG5014_cQED, \
+    AlazarTech_ATS9360_cQED, ATS9360Controller_cQED, VNA_cQED
 
 from qcodes.instrument_drivers.oxford.mercuryiPS import MercuryiPS
 from qcodes.instrument_drivers.rohde_schwarz.SGS100A import RohdeSchwarz_SGS100A
@@ -51,26 +51,37 @@ if __name__ == '__main__':
     calib_config = get_config('calib')
 
     # Initialise intruments
-    deca = Decadac_T2('Decadac', 'ASRL1::INSTR', instr_config)
-    alazar = AlazarTech_ATS9360_T3('alazar', seq_mode='off')
-    ave_ctrl = ATS9360Controller_T3('ave_ctrl', alazar, ctrl_type='ave')
-    rec_ctrl = ATS9360Controller_T3('rec_ctrl', alazar, ctrl_type='rec')
-    samp_ctrl = ATS9360Controller_T3('samp_ctrl', alazar, ctrl_type='samp')
-    localos = RohdeSchwarz_SGS100A('localos_rs',
-                                   'TCPIP0::192.168.15.104::inst0::INSTR')
-    cavity_source = RohdeSchwarz_SGS100A('cavity_rs',
-                                         'TCPIP0::192.168.15.105::inst0::INSTR')
-    qubit_source = RohdeSchwarz_SGS100A('qubit_source',
-                                        'TCPIP0::192.168.15.105::inst0::INSTR')
-    awg1 = AWG5014_T3(
+    deca = Decadac_cQED('Decadac', 'ASRL1::INSTR', instr_config)
+    STATION.add_component(deca)
+    alazar = AlazarTech_ATS9360_cQED('alazar', seq_mode='off')
+    STATION.add_component(alazar)
+    ave_ctrl = ATS9360Controller_cQED('ave_ctrl', alazar, ctrl_type='ave')
+    STATION.add_component(ave_ctrl)
+    rec_ctrl = ATS9360Controller_cQED('rec_ctrl', alazar, ctrl_type='rec')
+    STATION.add_component(rec_ctrl)
+    samp_ctrl = ATS9360Controller_cQED('samp_ctrl', alazar, ctrl_type='samp')
+    STATION.add_component(samp_ctrl)
+    localos = RohdeSchwarz_SGS100A(
+        'localos_rs', 'TCPIP0::192.168.15.104::inst0::INSTR')
+    STATION.add_component(localos)
+    cavity_source = RohdeSchwarz_SGS100A(
+        'cavity_rs', 'TCPIP0::192.168.15.105::inst0::INSTR')
+    STATION.add_component(cavity_source)
+    qubit_source = RohdeSchwarz_SGS100A(
+        'qubit_source', 'TCPIP0::192.168.15.105::inst0::INSTR')
+    STATION.add_component(qubit_source)
+    awg1 = AWG5014_cQED(
         'awg1', 'TCPIP0::192.168.15.101::inst0::INSTR', timeout=40)
-    awg2 = AWG5014_T3(
+    STATION.add_component(awg1)
+    awg2 = AWG5014_cQED(
         'awg2', 'TCPIP0::192.168.15.101::inst0::INSTR', timeout=40)
-    vna = VNA_T3('VNA', 'TCPIP0::192.168.15.103::inst0::INSTR', S21=True)
+    STATION.add_component(awg2)
+    vna = VNA_cQED('VNA', 'TCPIP0::192.168.15.103::inst0::INSTR',
+                   S21=True)
+    STATION.add_component(vna)
     dummy_time = ManualParameter('dummy_time')
 
-    # Specify which parameters are to be added to the monitir and printed in metadate
-    # The instuments they beong to will be added to the STATION
+    # Specify parameters to be added to the monitor and printed in metadata
     param_monitor_list = [
         deca.channels[0], deca.channels[1],
         deca.channels[2], deca.channels[3],
@@ -88,12 +99,6 @@ if __name__ == '__main__':
         vna.channels.S21.power, vna.channels.S21.start,
         vna.channels.S21.stop, vna.channels.S21.avg,
         vna.channels.S21.bandwidth, vna.channels.S21.npts]
-
-    # Add instruments to station so that metadata for them is recorded at
-    # each measurement and connections are closed at end of session
-    for param in param_monitor_list:
-        if param._instrument.name not in STATION.components.keys():
-            STATION.add_component(param._instrument)
 
     # Set log level
     logger = logging.getLogger()
