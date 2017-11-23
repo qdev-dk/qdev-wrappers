fad456134010be70a5f00d586111a7a9aad6a54c
Thorvald Larsen <thorvaldw@hotmail.com>
2017-11-23 14:40:29 +0100

    Init file
    Changed init file

--- qdev_wrappers/cQED/Experiment_init_high_freq.py
+++ qdev_wrappers/cQED/Experiment_init_high_freq.py
@@ -1,145 +1,111 @@
 import qcodes as qc
 import time
-import sys
 import logging
 import numpy as np
-from os.path import sep
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-mpl.rcParams['figure.figsize'] = (8, 3)
 mpl.rcParams['figure.subplot.bottom'] = 0.15
-mpl.rcParams['font.size'] = 8
+mpl.rcParams['font.size'] = 10
 
-from qdev_wrappers.file_setup import CURRENT_EXPERIMENT
+from qdev_wrappers.file_setup import CURRENT_EXPERIMENT, my_init
 from qdev_wrappers.configreader import Config
-from qdev_wrappers.file_setup import close_station, my_init
-from qcodes import ManualParameter
-
-from qdev_wrappers import *
-from qdev_wrappers.majorana.majorana_wrappers import *
-from qdev_wrappers.majorana.reload_settings import *
-from qdev_wrappers.T3.customised_instruments import SR830_T3, Decadac_T3, AWG5014_T3, \
-ATS9360Controller_T3, AlazarTech_ATS9360_T3, VNA_T3
-helper_fns_folder = r'D:\Transmon\Qcodes-contrib'
-if helper_fns_folder not in sys.path:
-    sys.path.insert(0, helper_fns_folder)
+from qdev_wrappers.show_num import show_num, show_meta
+from qdev_wrappers.sweep_functions import do1d, do2d, do0d
 from qdev_wrappers.transmon import *
+from chickpea import Segment, Waveform, Element, Sequence
 
+# import instrument drivers
+from qcodes.instrument_drivers.agilent.Agilent_34400A import Agilent_34400A
+from qcodes.instrument_drivers.QDev.QDac import QDac
 from qcodes.instrument_drivers.oxford.mercuryiPS import MercuryiPS
 from qcodes.instrument_drivers.rohde_schwarz.SGS100A import RohdeSchwarz_SGS100A
+from qcodes.instrument_drivers.yokogawa.GS200 import GS200
+from qdev_wrappers.cQED.customised_instruments import SR830_qQED, Decadac_cQED, AWG5014_cQED, \
+ VNA_cQED, ATS9360Controller_cQED, AlazarTech_ATS9360_cQED, GS200_cQED, Keithley_2600_cQED, SphereCor
 
-from qdev_wrappers.majorana.conductance_measurements import do2Dconductance
+
 
 if __name__ == '__main__':
-
+    
+    # Set up logger
     init_log = logging.getLogger(__name__)
-
-    # Close existing connections if present
-    if qc.Station.default:
-        close_station(qc.Station.default)
-
+    logger = logging.getLogger()
+    logger.setLevel(logging.WARNING)
+    
+    # initialise station
     STATION = qc.Station()
 
     # Set up folders, settings and logging for the experiment
-    my_init("AcQED_05_98_dev1", STATION,
-            pdf_folder=True, analysis_folder=True,
-            waveforms_folder=True,
-            annotate_image=False, mainfolder=None, display_pdf=True,
+    my_init("MT_LEc03_SE_sw", STATION,
+            pdf_folder=True, png_folder=True, analysis_folder=True,
+            waveforms_folder=True, calib_config=True,
+            annotate_image=False, mainfolder=None, display_pdf=False,
             display_individual_pdf=False, qubit_count=1,
             plot_x_position=0.66)
 
-    # Load config from experiment file, if none found then uses one in mainfolder
-    cfg_file = "{}{}".format(CURRENT_EXPERIMENT['exp_folder'], 'instr.config')
-    instr_config = Config(cfg_file, isdefault=True)
-    if len(instr_config.sections()) == 0:
-        cfg_file = sep.join([CURRENT_EXPERIMENT['mainfolder'], 'instr.config'])
-        instr_config = Config(cfg_file, isdefault=True)
-        
+    # Load config from experiment file
+    instr_config = get_config('instr')
 
     # Initialise intruments
-    deca = Decadac_T3('Decadac', 'ASRL1::INSTR', instr_config)
-#    lockin_2 = SR830_T3('lockin_2', 'GPIB0::2::INSTR', instr_config)
-    alazar = AlazarTech_ATS9360_T3('alazar', seq_mode='off')
-    ave_ctrl = ATS9360Controller_T3('ave_ctrl', alazar, ctrl_type='ave')
-    rec_ctrl = ATS9360Controller_T3('rec_ctrl', alazar, ctrl_type='rec')
-    samp_ctrl = ATS9360Controller_T3('samp_ctrl', alazar, ctrl_type='samp')
-    localos = RohdeSchwarz_SGS100A('localos_rs',
-                                   'TCPIP0::192.168.15.104::inst0::INSTR')
-    cavity = RohdeSchwarz_SGS100A('cavity_rs',
-                                  'TCPIP0::192.168.15.105::inst0::INSTR')
-    awg = AWG5014_T3('awg', 'TCPIP0::192.168.15.101::inst0::INSTR', timeout=40)
+    qdac = QDac('qDac','ASRL4::INSTR'); STATION.add_component(qdac)
+    dmm1 = Agilent_34400A('DMM1','GPIB0::11::INSTR'); STATION.add_component(dmm1)
+    dmm2 = Agilent_34400A('DMM2','GPIB0::8::INSTR'); STATION.add_component(dmm2)
+#    deca = Decadac_T3('Decadac', 'ASRL1::INSTR', instr_config); STATION.add_component(deca)
+#    lockin_2 = SR830_T3('lockin_2', 'GPIB0::2::INSTR', instr_config); STATION.add_component(lockin_2)
+    keith = Keithley_2600_cQED('Keithley','GPIB0::9::INSTR','a'); STATION.add_component(keith)
+#    gs200 = GS200_cQED('gs200','GPIB0::10::INSTR'); STATION.add_component(gs200)
+    alazar = AlazarTech_ATS9360_cQED('alazar', seq_mode='off'); STATION.add_component(alazar)
+    ave_ctrl = ATS9360Controller_cQED('ave_ctrl', alazar, ctrl_type='ave'); STATION.add_component(ave_ctrl)
+    rec_ctrl = ATS9360Controller_cQED('rec_ctrl', alazar, ctrl_type='rec'); STATION.add_component(rec_ctrl)
+    samp_ctrl = ATS9360Controller_cQED('samp_ctrl', alazar, ctrl_type='samp'); STATION.add_component(samp_ctrl)
+    qubit = RohdeSchwarz_SGS100A('qubit', 'TCPIP0::192.168.15.101::inst0::INSTR'); STATION.add_component(qubit)
+    localos = RohdeSchwarz_SGS100A('localos', 'TCPIP0::192.168.15.105::inst0::INSTR'); STATION.add_component(localos)
+    cavity = RohdeSchwarz_SGS100A('cavity', 'TCPIP0::192.168.15.104::inst0::INSTR'); STATION.add_component(cavity)
+    awg = AWG5014_T3('awg', 'TCPIP0::192.168.15.102::inst0::INSTR', timeout=40); STATION.add_component(awg)
     mercury = MercuryiPS(name='mercury',
-                         address='172.20.10.148',
+                         address='192.168.15.200',
                          port=7020,
-                         axes=['X', 'Y', 'Z'])
-    vna = VNA_T3('VNA', 'TCPIP0::192.168.15.103::inst0::INSTR')
-    dummy_time = ManualParameter('dummy_time')
+                         axes=['X', 'Y', 'Z']); STATION.add_component(mercury)
+    vna = VNA_T3('VNA', 'TCPIP0::192.168.15.103::inst0::INSTR'); STATION.add_component(vna)
+    dummy_time = qc.ManualParameter('dummy_time')
+    mag_sphere = SphereCor('Magnet',mercury.x_fld,keith.By,mercury.z_fld); STATION.add_component(mag_sphere)
+    mag_sphere.radius.label = 'B magnitude'
+    mag_sphere.radius.unit = 'T'
+    mag_sphere.theta.label = 'B theta'
+    mag_sphere.phi.label = 'B phi'
 
-    # Add instruments to station so that metadata for them is recorded at
-    # each measurement and connections are closed at end of session
-    STATION.add_component(deca)
-#    STATION.add_component(lockin_2)
-    STATION.add_component(vna)
-    STATION.add_component(mercury)
-    STATION.add_component(alazar)
-    STATION.add_component(ave_ctrl)
-    STATION.add_component(rec_ctrl)
-    STATION.add_component(samp_ctrl)
-    STATION.add_component(localos)
-    STATION.add_component(cavity)
-    STATION.add_component(awg)
+    # Specify which parameters are to be added to the monitir and printed in metadate
+    # The instuments they beong to will be added to the STATION
+    param_monitor_list = [
+        qdac.ch01_v, qdac.ch02_v, qdac.ch03_v, qdac.ch04_v,
+        samp_ctrl.num_avg, samp_ctrl.int_time, samp_ctrl.int_delay,
+        rec_ctrl.num_avg, rec_ctrl.int_time, rec_ctrl.int_delay,
+        ave_ctrl.num_avg, ave_ctrl.int_time, ave_ctrl.int_delay,
+        awg.state, awg.ch1_amp, awg.ch2_amp, 
+        awg.ch3_amp, awg.ch4_amp,
+        alazar.seq_mode,
+        qubit.frequency, qubit.power, qubit.status, qubit.IQ_state,
+        localos.frequency, localos.power, localos.status,
+        cavity.frequency, cavity.power, cavity.status, cavity.IQ_state,
+        vna.channels.S21.power, vna.channels.S21.start,
+        vna.channels.S21.stop, vna.channels.S21.avg,
+        vna.channels.S21.bandwidth, vna.channels.S21.npts,
+        mercury.x_fld, mercury.z_fld, keith.By,
+        mag_sphere.radius, mag_sphere.theta, mag_sphere.phi
+        ]
 
-    # Set log level
-    logger = logging.getLogger()
-    logger.setLevel(logging.WARNING)
 
     # Get parameter values to populate monitor
     print('Querying all instrument parameters for metadata.'
           'This may take a while...')
     start = time.time()
 
-#    lockin_2.acbias()
-    deca.dcbias.get()
-    deca.lcut.get()
-    deca.rcut.get()
-    deca.jj.get()
-    deca.rplg.get()
-    deca.lplg.get()
-    mercury.x_fld()
-    mercury.y_fld()
-    mercury.z_fld()
-    vna.rf_power()
-    vna.channels.S21.npts()
-    vna.channels.S21.power()
-    vna.channels.S21.start()
-    vna.channels.S21.stop()
-    vna.channels.S21.avg()
-    vna.channels.S21.bandwidth()
-    cavity.status()
-    cavity.power()
-    cavity.frequency()
-    localos.status()
-    localos.power()
-    localos.frequency()
-    awg.state()
-    awg.ch1_amp()
-    awg.ch1_state()
+    for param in param_monitor_list:
+        param.get()
 
     end = time.time()
 
-    print("done Querying all instruments took {}".format(end - start))
-
+    print("done Querying all instruments took {}".format(end - start))    
+    
     # Put parameters into monitor
-    Monitor(mercury.x_fld, mercury.y_fld, mercury.z_fld,
-               deca.dcbias, deca.lcut, deca.rcut, deca.jj, deca.rplg,
-               deca.lplg,
-#               lockin_2.acbias,
-               samp_ctrl.num_avg, samp_ctrl.int_time, samp_ctrl.int_delay,
-               rec_ctrl.num_avg, rec_ctrl.int_time, rec_ctrl.int_delay,
-               ave_ctrl.num_avg, ave_ctrl.int_time, ave_ctrl.int_delay,
-               awg.state, awg.ch1_amp, awg.ch1_state, alazar.seq_mode,
-               cavity.frequency, localos.frequency, cavity.power,
-               localos.power, cavity.status, localos.status,
-               vna.channels.S21.power, vna.channels.S21.start,
-               vna.channels.S21.stop, vna.channels.S21.avg,
-               vna.channels.S21.bandwidth, vna.channels.S21.npts)
+    qc.Monitor(*param_monitor_list)
