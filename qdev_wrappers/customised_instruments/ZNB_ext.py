5cbaeb601b802bb87c7a317e09e220410971a5e2
Dominik Vogel <a-dovoge@microsoft.com>
2017-12-06 17:50:11 +0100

    Frequency sweep to cavity peak

---
+++ qdev_wrappers/customised_instruments/ZNB_ext.py
@@ -0,0 +1,40 @@
+from qcodes.instrument_drivers.rohde_schwarz.ZNB import ZNB, FrequencySweep, ZNBChannel
+
+class FrequencySweepMagSetCav(ArrayParameter):
+    FORMAT = 'dB'
+
+    def get(self):
+        # this could also be set here instead of checking
+        if self._instrument.format() != self.FORMAT:
+            raise RuntimeError(
+                'Channel format must be set to {} first'.format(self.FORMAT))
+        mag_array = super().get()
+
+        ind = np.argmax(mag_array)
+        f = tuple(np.linspace(int(self._instrument.start()),
+                              int(self._instrument.stop()),
+                              num=self._instrument.npts()))
+        freadout = f[ind] + 0.7e6
+
+        self._instrument._parent.readout_freq(freadout)
+        return mag_array
+
+
+class ZNBChannel_ext(ZNBChannel):
+    def __init__(self, parent, name, channel):
+        super().__init__(parent, name, channel)
+
+        self.add_parameter(name='trace_mag_SetCav',
+                           start=self.start(),
+                           stop=self.stop(),
+                           npts=self.npts(),
+                           channel=n,
+                           parameter_class=FrequencySweepMagSetCav)
+
+
+class VNA_ext(ZNB):
+
+    CHANNEL_CLASS = ZNBChannel_ext
+
+
+
