5105ba5cc25ec7e2389c08abc88c5c363b9108bf
Thorvald Larsen <thorvaldw@hotmail.com>
2017-12-10 12:32:22 +0100

    Reformat to classes

--- qdev_wrappers/qdev_fitter.py
+++ qdev_wrappers/qdev_fitter.py
@@ -6,136 +6,131 @@ import matplotlib.pyplot as plt
 import scipy.fftpack as fftpack
 from scipy.optimize import curve_fit
 
-from qdev_wrappers.file_setup import CURRENT_EXPERIMENT
+from qdev_wrappers.show_num import check_experiment_is_initialized
 
+class qdev_fitter():
+    def __init__(self):
+        self.T1 = T1()
+        self.T2 = T2()
 
+    def fit(self, id, fitclass, do_plots=True, dataname=None, samplefolder=None, p0=None,**kwargs):
 
+        str_id = '{0:03d}'.format(id)
+        if samplefolder==None:
+            check_experiment_is_initialized()
 
-def qdev_fitter(id, fitfunction_in, do_plots=True, dataname=None, samplefolder=None, p0=None,**kwargs):
-    '''
-    Function to allow easy on the fly fitting of data. Only works for 1D datasets at the moment.
-    id (num): Id of dataset
-    fitfunction_in (str, function): Function data will be fitted to. Can be string for functions defined here or a function passed direclty defined elsewhere.
-    do_plots (bool): If plots are done. Will be saved in the analysis folder for the samplepath
-    dataname (str): String with dataname in the qcodes dataset
-    samplefolder (path): Path to samplefolder if not opening active measurements
-    p0 (tuple): Initial parameters for fit
-    kwargs: Passed to scipy.optimize.curve_fit when fitting.
-    '''
-
-    # Load data from active samplefolder or input samplefolder.
-    str_id = '{0:03d}'.format(id)
-    if samplefolder==None:
-        check_experiment_is_initialized()
-
-        path = qc.DataSet.location_provider.fmt.format(counter=str_id)
-        data = qc.load_data(path)
-    else:
-        path = '{}{}{}'.format(samplefolder,sep,str_id)
-        data = qc.load_data(path)
-
-    # Get dataname to be fitted
-    if dataname==None:
-        # Function only made for a single dataname. Check there isnt more datanames in the dataset.
-        key_data = [key for key in data.arrays.keys() if "_set" not in key[-4:]]
-        if len(key_data) != 1:
-            raise RuntimeError('Dataset has more than one dataname. Choose dataname to be fitted from: {}.'.format(', '.join(key_data)))
-    else:
-        # Check input dataname is in dataset.
-        if dataname not in [key for key in data.arrays.keys() if "_set" not in key[-4:]]:
-            raise RuntimeError('Dataname not in dataset. Input dataname was: \'{}\' while dataname(s) in dataset are: {}.'.format(dataname,', '.join(key_data)))
-        key_data = [dataname]
-
-    # Get handle for fitting function
-    if isinstance(fitfunction_in,str):
-        if fitfunction_in not in list(globals().keys()):
-            raise RuntimeError('Fitfunction not defined.')
-        fitfunction = globals()['{}'.format(fitfunction_in)]
-    elif isinstance(fitfunction_in,types.FunctionType):
-        fitfunction = fitfunction_in
-    else:
-        print('Invalid input. Only takes strings of predefined fucntions or function input.')
-
-
-    # Do fits separately for 1D and 2D datasets
-    keys_set = [key for key in data.arrays.keys() if "_set" in key[-4:]]
-
-    # Do fit and plot for 1D data
-    if len(keys_set) == 1:
-        xdata = getattr(getattr(data, keys_set[0]), 'ndarray')
-        ydata = getattr(getattr(data, key_data[0]), 'ndarray')
-        # Get initial guess on parameter is guess function is defined
-        if (p0==None and '{}_guess'.format(fitfunction_in) in list(globals().keys())):
-            p0 = globals()['{}_guess'.format(fitfunction_in)](xdata,ydata)
-        popt, pcov = popt, pcov = curve_fit(fitfunction, xdata, ydata, p0=p0, **kwargs)
-
-        if do_plots:
-            plot = plot_1D(getattr(data, keys_set[0]),getattr(data, key_data[0]),fitfunction,popt)
-            title_list = plot.get_default_title().split(sep)
-            title_list.insert(-1, 'analysis')
-            title = sep.join(title_list)
-            plt.savefig("{}_{}.png".format(title,fitfunction.__name__),dpi=500)
-            return popt, pcov, plot
+            path = qc.DataSet.location_provider.fmt.format(counter=str_id)
+            data = qc.load_data(path)
         else:
-            return popt, pcov
+            path = '{}{}{}'.format(samplefolder,sep,str_id)
+            data = qc.load_data(path)
 
-    # Not implemented
-    if len(keys_set) == 2:
-        if xname==None:
-            raise RuntimeError('Need fitting dimension for 2D datasets. Choose fit dimension from: {}.'.format(', '.join(keys_set)))
-        raise RuntimeError('Fitting not ready for 2D datasets.')
+        # Get dataname to be fitted
+        if dataname==None:
+            # Function only made for a single dataname. Check there isnt more datanames in the dataset.
+            key_data = [key for key in data.arrays.keys() if "_set" not in key[-4:]]
+            if len(key_data) != 1:
+                raise RuntimeError('Dataset has more than one dataname. Choose dataname to be fitted from: {}.'.format(', '.join(key_data)))
+        else:
+            # Check input dataname is in dataset.
+            if dataname not in [key for key in data.arrays.keys() if "_set" not in key[-4:]]:
+                raise RuntimeError('Dataname not in dataset. Input dataname was: \'{}\' while dataname(s) in dataset are: {}.'.format(dataname,', '.join(key_data)))
+            key_data = [dataname]
+
+        # Do fits separately for 1D and 2D datasets
+        keys_set = [key for key in data.arrays.keys() if "_set" in key[-4:]]
+
+        # Do fit and plot for 1D data
+        if len(keys_set) == 1:
+            qcxdata = getattr(data, keys_set[0])
+            qcydata = getattr(data, key_data[0])
+            # Get initial guess on parameter is guess function is defined
+            if (p0==None and hasattr(fitclass,'guess')):
+                p0 = getattr(fitclass,'guess')(qcxdata.ndarray,qcydata.ndarray)
+            popt, pcov = popt, pcov = curve_fit(fitclass.fun, qcxdata.ndarray, qcydata.ndarray, p0=p0, **kwargs)
+
+            if do_plots:
+                plot = self.plot_1D(qcxdata,qcydata,fitclass,popt)
+                title_list = plot.get_default_title().split(sep)
+                title_list.insert(-1, 'analysis')
+                title = sep.join(title_list)
+                plt.savefig("{}_{}.png".format(title,fitclass.name),dpi=500)
+                return popt, pcov, plot
+            else:
+                return popt, pcov
 
 
-def check_experiment_is_initialized():
-    if not getattr(CURRENT_EXPERIMENT, "init", True):
-        raise RuntimeError("Experiment not initalized. "
-                           "use qc.Init(mainfolder, samplename) "
-                           "or provide path to sample folder.")
-
-def plot_1D(qcxdata,qcydata,fitfunction,popt):
-    plot = qc.MatPlot(qcydata,marker='.',markersize=10,linestyle='')
-    xdata = getattr(qcxdata, 'ndarray')
-    x = np.linspace(xdata.min(),xdata.max(),len(xdata)*10)
-    plot[0].axes.plot(x,fitfunction(x,*popt),
-        label='\n'.join(['p{} = {:.3g}'.format(n,m) for n, m in enumerate(popt)]))
-    plot.rescale_axis()
-    plot[0].axes.legend(loc='upper right', fontsize=10)
-    plt.draw()
-    return plot
-
+    def plot_1D(self,qcxdata,qcydata,fitclass,popt):
+        plot = qc.MatPlot(qcydata,marker='.',markersize=5,linestyle='',color='C0')
+        plot.fig.tight_layout(pad=3)
+        plot.rescale_axis()
+        xdata = qcxdata.ndarray
+        p_label_list = []
+        for i in range(len(fitclass.p_names)):
+            ax_letter = fitclass.p_units[i]
+            if ax_letter in ['x','y']:
+                unit = getattr(plot.subplots[0], 'get_{}label'.format(ax_letter))().split('(')[1].split(')')[0]
+                scaled = float(getattr(plot.subplots[0], '{}axis'.format(ax_letter)).get_major_formatter()(popt[i]))
+            elif ax_letter in ['1/x','1/y']:
+                unit = '/{}'.format(getattr(plot.subplots[0], 'get_{}label'.format(ax_letter[2]))().split('(')[1].split(')')[0])
+                scaled = 1/float(getattr(plot.subplots[0], '{}axis'.format(ax_letter[2])).get_major_formatter()(1/popt[i]))
+            else:
+                unit = ax_letter
+                scaled = popt[i]
+            p_label_list.append('{} = {:.3g} {}'.format(fitclass.p_names[i],scaled,unit))
+        x = np.linspace(xdata.min(),xdata.max(),len(xdata)*10)
+        plot[0].axes.plot(x,fitclass.fun(x,*popt),color='C0',
+            label='\n'.join(p_label_list))
+        plot[0].axes.legend(loc='upper right')
+        plot.subplots[0].set_title(fitclass.fun_str)
+        plt.draw()
+        return plot
 
 
 #  Predefined fucntions
+class T1():
+    def __init__(self):
+        self.name = 'T1fit'
+        self.fun_str = r'$f = a \exp(-x/T) + c$'
+        self.p_names = [r'$a$',r'$T$',r'$c$']
+        self.p_units = ['y','x','y']
+
+    def fun(self,x,a,T,c):
+        val = a*np.exp(-x/T)+c
+        return val
+
+    def guess(self,x,y):
+        l = len(y)
+        val_init = y[0:round(l/20)].mean()
+        val_fin = y[-round(l/20):].mean()
+        a = val_init - val_fin
+        c = val_fin
+        # guess T1 as point where data has falen to 1/e of init value
+        idx = (np.abs(y-a/np.e-c)).argmin()
+        T = x[idx]
+        return [a,T,c]
 
 
-def T1(x,a,T,c):
-    val = a*np.exp(-x/T)+c
-    return val
+class T2():
+    def __init__(self):
+        self.name = 'T2fit'
+        self.fun_str = r'$f = a \sin(\omega x +\phi)\exp(-x/T) + c$'
+        self.p_names = [r'$a$',r'$T$',r'$\omega/2\pi$',r'$\phi$',r'$c$']
+        self.p_units = ['y','x','1/x','','y']
 
-def T1_guess(x,y):
-    l = len(y)
-    val_init = y[0:round(l/20)].mean()
-    val_fin = y[-round(l/20):].mean()
-    a = val_init - val_fin
-    c = val_fin
-    # guess T1 as point where data has falen to 1/e of init value
-    idx = (np.abs(y-a/np.e-c)).argmin()
-    T = x[idx]
-    return [a,T,c]
+    def fun(self,x,a,T,w,p,c):
+        val = a*np.exp(-x/T)*np.sin(w*x+p)+c
+        return val
 
-def T2(x,a,T,w,p,c):
-    val = a*np.exp(-x/T)*np.sin(w*x+p)+c
-    return val
-
-def T2_guess(x,y):
-    a = y.max() - y.min()
-    c = y.mean()
-    # guess T2 as point half way point in data
-    T = x[round(len(x)/2)]
-    # Get initial guess for frequency from a fourier transform
-    yhat = fftpack.rfft(y-y.mean())
-    idx = (yhat**2).argmax()
-    freqs = fftpack.rfftfreq(len(x), d = (x[1]-x[0])/(2*np.pi))
-    w = freqs[idx]
-    p = 0
-    return [a,T,w,p,c]
+    def guess(self,x,y):
+        a = y.max() - y.min()
+        c = y.mean()
+        # guess T2 as point half way point in data
+        T = x[round(len(x)/2)]
+        # Get initial guess for frequency from a fourier transform
+        yhat = fftpack.rfft(y-y.mean())
+        idx = (yhat**2).argmax()
+        freqs = fftpack.rfftfreq(len(x), d = (x[1]-x[0])/(2*np.pi))
+        w = freqs[idx]
+        p = 0
+        return [a,T,w,p,c]
