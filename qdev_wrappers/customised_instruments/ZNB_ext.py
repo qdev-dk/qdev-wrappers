9324cd39074abe452f1934321fcc426a329f6410
Dominik Vogel <a-dovoge@microsoft.com>
2017-12-11 09:54:00 +0100

    formating

--- qdev_wrappers/customised_instruments/ZNB_ext.py
+++ qdev_wrappers/customised_instruments/ZNB_ext.py
@@ -6,14 +6,16 @@ class FrequencySweepMagSetCav(ArrayParameter):
     def get(self):
         # this could also be set here instead of checking
         if self._instrument.format() != self.FORMAT:
-            raise RuntimeError(
-                'Channel format must be set to {} first'.format(self.FORMAT))
+            raise RuntimeError('Channel format must be set to {} first'.format(
+                self.FORMAT))
         mag_array = super().get()
 
         ind = np.argmax(mag_array)
-        f = tuple(np.linspace(int(self._instrument.start()),
-                              int(self._instrument.stop()),
-                              num=self._instrument.npts()))
+        f = tuple(
+            np.linspace(
+                int(self._instrument.start()),
+                int(self._instrument.stop()),
+                num=self._instrument.npts()))
         freadout = f[ind] + 0.7e6
 
         self._instrument._parent.readout_freq(freadout)
@@ -24,17 +26,15 @@ class ZNBChannel_ext(ZNBChannel):
     def __init__(self, parent, name, channel):
         super().__init__(parent, name, channel)
 
-        self.add_parameter(name='trace_mag_SetCav',
-                           start=self.start(),
-                           stop=self.stop(),
-                           npts=self.npts(),
-                           channel=n,
-                           parameter_class=FrequencySweepMagSetCav)
+        self.add_parameter(
+            name='trace_mag_SetCav',
+            start=self.start(),
+            stop=self.stop(),
+            npts=self.npts(),
+            channel=n,
+            parameter_class=FrequencySweepMagSetCav)
 
 
 class VNA_ext(ZNB):
 
     CHANNEL_CLASS = ZNBChannel_ext
-
-
-
