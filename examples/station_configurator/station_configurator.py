13b66a5665603b424e776d59d0be87596f66955d
Dominik Vogel <a-dovoge@microsoft.com>
2018-03-06 11:02:16 +0100

    fixed example

--- examples/station_configurator/station_configurator.py
+++ examples/station_configurator/station_configurator.py
@@ -28,15 +28,15 @@ mock_dac = scfg.load_instrument('mock_dac')
 # yaml file to point to the sim file
 # ls = scfg.load_instrument('lakeshore')
 
-if you happen to have a qdac you can also change the hardware address in the
-config file and then do the following:
-watch out! the current config file will set a voltage on the qdac!
-qdac = scfg.load_instrument('qdac')
-# now you should be able to do
-qdac.Bx(0.04)
-# which should ramp up the voltage of ch02 from 0.02V (initial value) to
-# 0.08V (scaling factor is 0.2)
-# that is fine
-qdac.Bx(0.09)
-# but this will fail because it is outside the specified range
-qdac.Bx(0.11)
+# # if you happen to have a qdac you can also change the hardware address in the
+# # config file and then do the following:
+# # watch out! the current config file will set a voltage on the qdac!
+# qdac = scfg.load_instrument('qdac')
+# # now you should be able to do
+# qdac.Bx(0.04)
+# # which should ramp up the voltage of ch02 from 0.02V (initial value) to
+# # 0.08V (scaling factor is 0.2)
+# # that is fine
+# qdac.Bx(0.09)
+# # but this will fail because it is outside the specified range
+# qdac.Bx(0.11)
