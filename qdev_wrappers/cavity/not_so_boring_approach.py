3ed979ee0a189d36e07fa7bdc9fe37610246ecd7
Dominik Vogel <a-dovoge@microsoft.com>
2018-03-08 18:55:26 +0100

    simplification

--- qdev_wrappers/cavity/not_so_boring_approach.py
+++ qdev_wrappers/cavity/not_so_boring_approach.py
@@ -1,42 +1,52 @@
-class ParametricPulseBuilder:
+from typing import Callable
 
-    def build_pulse(parameters: Dict[]) -> bb.Element:
-        pass
+# broadbean extentions
+def pushElement(self, element: bb.Element) -> None:
+    self.addElement(self.length_sequenceelements, element)
 
-    def serialize(stream: )
+bb.Sequence.pushElement = pushElement
 
 
-class ParametricSequence:
-"""
-    Attributes:
-         parameters (List[List[Dict]]): represents a sequence -> can become a strucutred array at some point
-         set_points_x, set_points_y: represents the axis of the parameter 2D List.
-         pulse_builder: function that takes the attributes to make some elements
+class ParametricSequencer:
+    """
+    Take a step back to make it more general, and keep the ParametricWaveforms in the background
+    Args:
+    builder:  f_with_footprint(buffer_index:int, buffer_setpoint:float,
+    record_index: int, record_setpoint:float) -> bb.Element
 
-"""
-    def __init__(channel_base_name,
-                 parameters: List[List[Dict]]=None,
-                 pulse_builder: ParametricPulseBuilder,
-                 set_points: Tuple[List[float], List[float]]=None):
-        self.channel_base_name = channel_base_name
-        self.parameters = parameters
-        self.pulse_builder = pulse_builder
+    """
+    def __init__(builder: Callable,
+                 integration_delay:float = None,
+                 integration_time:float = None,
+                 record_set_points:List[float] = None,
+                 buffer_set_points:List[float] = None):
+        # TODO: add units and labels
+        self.integration_delay = integration_delay
+        self.integration_time = integration_time
+        self.record_set_points = record_set_points
+        self.buffer_set_points = buffer_set_points
+
+        self.average_records = self.record_set_points is not None:
+        self.average_buffers = self.buffer_set_points is not None:
+        self.average_time = self.integration_delay is not None and self.integration_time is not None
 
     def create_sequence(self) -> bb.Sequence:
-        # sequence = broabbean magic with paramters
-        # consider caching at later stage
-        for list in self.parameters:
-            for shot in list:
-                sequence.append(self.pulse_builder(self.channel_base_name, shot))
+        # this is the simple and naïve way, without any repeat elements
+        # but one can simply add them here
+        sequence = bb.Sequence()
+        for buffer_index, buffer_set_point in enumerate(self.buffer_set_points):
+            for record_index, record_set_point in enumerate(self.record_set_points):
+                element = build_element(buffer_index, buffer_setpoint,
+                                        record_index, record_setpoint)
+                sequence.pushElement(element)
         return sequence
 
-    def serialize(self, stream):
-        # load from metadata stored in station
-        # needs fixed set of atomic puls builders
-        pass
+    # TODO: implement
+    # TODO: implement as stream
+    def serialize(self) -> str:
+        return "Not yet implemented"
 
-    def deserialize(self, stream):
-        # prepare for storing in station
+    def deserialize(self, str) -> None:
         pass
 
 
@@ -53,36 +63,23 @@ For that functionality it compises an AWG and a Alazar as a high speed ADC.
         self.station, self.awg, self.alazar = station, awg, alazar
 
 
-    def update_sequencer(self, sequencer: ParametricSequencer) -> None:
-        """
-        """
-        # save the current sequencer from parameters
-        self.sequencer = sequencer
-        station.components['sequencer'] = self.sequencer.store()  # see how this needs to be converted, to and from the json config
-
-    def set_up_acquisition(self, acquisition_parameters: Dict)
-        # this should be simple, everything from the alazar is a parameter, so it gets stored
-        self.acquisition_parameters = acquisition_parameters
-        pass
-
-    def run_experiment(self,
-                       before_each_record,
-                       after_each_record,
-                       before_each_buffer,
-                       after_each_buffer):
-        pass
-        # check if aqucisition mode agrees with actions
-
-
     # implementations
-    def upload_sequence(self, sequencer=None):
-        if sequencer is not None:
-            self.update_sequencer(sequencer)
+    def update_sequencer(self, sequencer):
+        self.sequencer = sequencer
+        station.components['sequencer'] = self.sequencer.serialize()  # see how this needs to be converted, to and from the json config
         seq = self.sequencer.create_sequence()
+        # set alazar in the right averaging mode
         # self.awg.upload(seq)
+        # self.awg.start
 
-def load_aquisition_parametes(metadata_from_station)->Dict:
-    pass
+
+    def get(self):
+        # must have called update_sequence before
+        # trigger alazar
+
+
+# def load_aquisition_parametes(metadata_from_station)->Dict:
+#     pass
 
 # implement shownums equivalent for acquisition parameters and sequencers
 
