b92955a73b95421298124ce3c46246d7983170e1
Dominik Vogel <a-dovoge@microsoft.com>
2018-03-09 16:49:02 +0100

    friday

--- qdev_wrappers/cavity/not_so_boring_approach.py
+++ qdev_wrappers/cavity/not_so_boring_approach.py
@@ -1,4 +1,5 @@
 from typing import Callable
+from copy import deepcopy
 
 # broadbean extentions
 def pushElement(self, element: bb.Element) -> None:
@@ -19,7 +20,9 @@ class ParametricSequencer:
                  integration_delay:float = None,
                  integration_time:float = None,
                  record_set_points:List[float] = None,
-                 buffer_set_points:List[float] = None):
+                 buffer_set_points:List[float] = None,
+                 sequencing_mode:bool =True,
+                 n_averages = 1):
         # TODO: add units and labels
         self.integration_delay = integration_delay
         self.integration_time = integration_time
@@ -50,7 +53,7 @@ class ParametricSequencer:
         pass
 
 
-class ParametricWaveformAnalysator:
+class ParametricWaveformAnalyser:
 """
 The PWA represents a composite instrument. It is similar to a spectrum analyzer, but instead of a sine wave it probes using waveforms described through a set of parameters.
 For that functionality it compises an AWG and a Alazar as a high speed ADC.
@@ -61,17 +64,50 @@ For that functionality it compises an AWG and a Alazar as a high speed ADC.
 """
     def __init__(station: Station=None, awg=None, alazar=None) -> None:
         self.station, self.awg, self.alazar = station, awg, alazar
-
+        # create alazar controller
+        self.alazar_controller = ATSChannelController(name='PWA_controller',
+                                                      alazar_name='Alazar')
+        self.alazar_channel = None
 
     # implementations
     def update_sequencer(self, sequencer):
         self.sequencer = sequencer
         station.components['sequencer'] = self.sequencer.serialize()  # see how this needs to be converted, to and from the json config
         seq = self.sequencer.create_sequence()
-        # set alazar in the right averaging mode
+
+
         # self.awg.upload(seq)
         # self.awg.start
 
+    def setup_alazar(self, f_demod = None):
+        # Magnitude, phase,I, Q?
+        # need to be called when setting a new sequencer
+        # set alazar in the right averaging mode
+        if self.alazar_channel is not None:
+            del self.alazar_channel
+            # TODO: try to remove it from the channel list
+            # self.alazar_controller.channels.remove(chan_m)
+            # remove all channels
+        # setup controller
+        self.alazar_controller.int_time(self.sequencer.integration_time)
+        self.alazar_controller.int_delay(self.sequencer.integration_delay)
+        # setup channels
+        chan_m = AlazarChannel(self.alazar_controller,
+                             demod=f_demod is not None,
+                             average_buffers = self.sequencer.average_buffers,
+                             average_records = self.sequencer.average_records,
+                             integrate_samples = self.sequencer.average_time)
+        chan_m.demod_freq(f_demod)
+        chan_m.num_averages(self.sequencer.n_averages)
+        chan_m.prepare_channel() # sets setpoints and labels
+        # this is a problem, can I not get magnitude and phase at the same time?
+        chan_p = deepcopy(chan_m)
+        chan_m.demod_type('magnitude')
+        chan_p.demod_type('phase')
+        # set the labels correctly here chan_m.data.setpoint_labels/units setpoints....
+        # data is MultidimParameter
+        self.alazar_controller.channels.append(chan_m)
+        self.alazar_channel = chan_m
 
     def get(self):
         # must have called update_sequence before
