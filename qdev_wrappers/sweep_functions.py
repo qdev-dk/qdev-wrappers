ff0648c670e35b1cdca25b2852f85c72665c5a2d
Dominik Vogel <a-dovoge@microsoft.com>
2018-03-22 15:42:47 +0100

    fixed do2d for multiparameter

--- qdev_wrappers/sweep_functions.py
+++ qdev_wrappers/sweep_functions.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 from os.path import sep
 from typing import Optional, Tuple, Sequence
+from collections import Iterable
 from pyqtgraph.multiprocess.remoteproxy import ClosedError
 
 import qcodes as qc
@@ -333,6 +334,10 @@ def do2d(inst_set, start, stop, num_points, delay,
 
     for inst in inst_meas:
         if getattr(inst, "setpoints", False):
+            setpoints = inst.setpoints
+            if isinstance(setpoints, Iterable):
+                if all(len(v) == 0 for v in setpoints):
+                    continue
             raise ValueError("3d plotting is not supported")
 
     actions = []
