4bafde8ceba45c6a9415c3a72d7c50e558a20db9
Dominik Vogel <a-dovoge@microsoft.com>
2018-04-16 12:43:15 +0200

    capital INFO

--- qdev_wrappers/logger.py
+++ qdev_wrappers/logger.py
@@ -26,7 +26,7 @@ def start_python_logger() -> None:
     try:
         filelogginglevel = config.core.file_loglevel
     except KeyError:
-        filelogginglevel = "Info"
+        filelogginglevel = "INFO"
     consolelogginglevel = config.core.loglevel
     ch = logging.StreamHandler()
     ch.setLevel(consolelogginglevel)
