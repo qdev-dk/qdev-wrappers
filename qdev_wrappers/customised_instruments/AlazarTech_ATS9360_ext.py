c45f4c0de7d75175c84fc54f86503fbf796f42c8
ThorvaldLarsen <thorvaldw@hotmail.com>
2018-05-16 15:31:46 +0200

    Remove value errors and init part

--- qdev_wrappers/customised_instruments/AlazarTech_ATS9360_ext.py
+++ qdev_wrappers/customised_instruments/AlazarTech_ATS9360_ext.py
@@ -4,19 +4,11 @@ from qcodes.utils import validators as vals
 
 class AlazarTech_ATS9360_ext(AlazarTech_ATS9360):
     def __init__(self, name, seq_mode='off'):
-        if seq_mode == 'on':
-            io_mode = 'AUX_IN_TRIGGER_ENABLE'
-            io_param = 'TRIG_SLOPE_POSITIVE'
-        elif seq_mode == 'off':
-            io_mode = 'AUX_IN_AUXILIARY'
-            io_param = 'NONE'
-        else:
-            raise ValueError('must set seq mode to "on" or '
-                             '"off", received {}'.format(seq_mode))
         super().__init__(name=name)
         self.add_parameter(name='seq_mode',
                            get_cmd=self._get_seq_mod,
                            set_cmd=self._set_seq_mode,
+                           value=seq_mode,
                            vals=vals.Enum('on', 'off')
                            )
 
@@ -38,6 +30,4 @@ class AlazarTech_ATS9360_ext(AlazarTech_ATS9360):
         elif mode == 'off':
             self.aux_io_mode('AUX_IN_AUXILIARY')
             self.aux_io_param('NONE')
-        else:
-            raise ValueError('must set seq mode to "on" or "off"')
         self.sync_settings_to_card()
