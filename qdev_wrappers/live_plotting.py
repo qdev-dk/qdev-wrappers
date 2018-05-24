e7204516f7d4c2df4787113b0989708bd5c6ff15
Esteban A. Martinez <esteban.martinez@nbi.ku.dk>
2018-05-24 10:00:11 +0200

    Plot the last run_id even if it has already started. Save plots automatically.

--- qdev_wrappers/live_plotting.py
+++ qdev_wrappers/live_plotting.py
@@ -6,6 +6,7 @@ It will not work on a Spyder console because of multiprocessing voodoo.'''
 
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import matplotlib.animation as animation
 from qcodes.dataset.plotting import plot_by_id
 from qcodes.dataset.data_set import load_by_id
@@ -13,6 +14,9 @@ from qcodes.dataset.experiment_container import (load_last_experiment,
                                                  load_experiment)
 from multiprocessing import Process, Pool
 import time
+from qcodes.config.config import Config
+import os.path
+import os
 
 
 def make_title_from_id(run_id):
@@ -76,22 +80,56 @@ def prepare_figure(run_id):
     return fig, axes, cbars
     
 
-def run_animation(run_id, interval=1.):
+def make_filename(run_id):
+    extension = 'pdf'
+    db_path = Config()['core']['db_location']
+    db_folder = os.path.dirname(db_path)
+    plot_folder_name = 'plots'
+    plot_folder = os.path.join(db_folder, plot_folder_name)
+    os.makedirs(plot_folder, exist_ok=True)
+    filename = '{}.{}'.format(run_id, extension)
+    plot_path = os.path.join(plot_folder, filename)
+    return plot_path
+    
+
+def is_completed(run_id):
+    '''Check if the run from run_id has already finished.
+    
+    TODO: this function should check for dataset.completed instead of a new
+    run, but dataset.completed is currently broken.'''
+    
+    return is_there_new_run(run_id)
+
+
+def save_figure(filename):
+    '''Save the current figure at filename.'''
+    try:
+        plt.savefig(filename, bbox_inches='tight')
+        print('Saved plot at {}.'.format(filename))
+    except PermissionError:
+        print('File {} already exists, did not save plot.'.format(filename))
+
+
+def run_animation(run_id, interval=1., save=True):
     '''Run an animation from run_id.'''
+    plt.ioff()
+    plt.rcParams.update({'figure.max_open_warning': 0})
     print('Started plotting run_id {}.'.format(run_id))
     fig, axes, cbars = prepare_figure(run_id)
+    if save:
+        if type(save) is str:
+            filename = save
+        else:
+            filename = make_filename(run_id)
     
     def get_frame():
-        '''Keep iterating the animation as long as there isn't a new run.
+        '''Keep iterating the animation as long as there isn't a new run.'''
         
-        If there is a new run, but auto_close == False, keep iterating the
-        animation without updating it ('freeze' mode).
-        TODO: this function should check for dataset.completed instead of a new
-        run, but dataset.completed is currently broken.'''
-        
-        while not is_there_new_run(run_id):
+        while not is_completed(run_id):
             yield 'active'
         print('Stopped plotting run_id {}.'.format(run_id))
+        if save:
+            save_figure(filename)
     
     anim = animation.FuncAnimation(fig, refresh, get_frame,
                                    interval=interval * 1000,
@@ -101,16 +139,22 @@ def run_animation(run_id, interval=1.):
     print('Closed plot for run_id {}.'.format(run_id))
     
     
-def listen(interval=1.):
-    '''Listen for a new run, then spawn an animation and a new listener.'''        
-    plt.ioff()
+def listen(interval=1., save=True, current=True):
+    '''Listen for a new run, then spawn an animation and a new listener.
+    
+    The current parameter is a workaround for dataset.completed not working.
+    If current == True, the last run_id will be plotted even if it was run
+    before the daemon started. It will be replaced with a check whether the
+    last run_id is still running or not.'''
+    
     last_run_id = get_last_run_id()
     print('Listening for new run...')
-    while not is_there_new_run(last_run_id):
+    while not is_there_new_run(last_run_id) and not current:
         time.sleep(interval)
     new_run_id = get_last_run_id()
-    p_animate = Process(target=run_animation, args=(new_run_id, interval))
-    p_listen = Process(target=listen, args=(interval, ))
+    p_animate = Process(target=run_animation, args=(new_run_id, interval,
+                                                    save))
+    p_listen = Process(target=listen, args=(interval, save, False))
     p_animate.start()   
     p_listen.start()
     p_animate.join()
