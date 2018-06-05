55c2081d92d5095e3c6dca1716495faa7e7cb2e1
Lillian Austin <lillian542@gmail.com>
2018-06-05 19:25:44 +0200

    converter for legacy and SQL data to dictionary, fitter to find fit from dictionary

---
+++ qdev_wrappers/fitting/Converter.py
@@ -0,0 +1,210 @@
+import qcodes as qc
+import numpy as np
+from os.path import sep
+from qcodes.dataset.data_export import get_data_by_id
+from qcodes.dataset.sqlite_base import connect
+from qdev_wrappers.file_setup import CURRENT_EXPERIMENT
+from qdev_wrappers.show_num import check_experiment_is_initialized
+
+
+
+class DataConverter():
+    
+    def convert(self, id, samplefolder = None):
+        
+        data = self.find_data(id, samplefolder) 
+        data_dict = self.make_data_dictionary(data)
+        
+        dependencies = self.find_dependencies(id, data)  #SQL needs only id number to find dependencies, Legacy needs only data
+        all_variables = self.find_variables(data)
+        
+        data_dict['data_id'] = id
+        data_dict['dependencies'] = dependencies
+        data_dict['variables'] = all_variables
+        
+        return data_dict
+    
+
+
+class SQL_Converter(DataConverter):
+    
+    def find_data(self, id, samplefolder):  
+        
+            '''sample folder does nothing except act as placeholder.                    
+            This function currently proceeds under the assumption that the experiment is set up, and doesn't
+            check anything to confirm that. Also, I think this doesn't return any error if it can't find 
+            the dataset with that id number.'''
+          
+            data = get_data_by_id(id)                   
+            
+            return data  
+
+        
+    def find_dependencies(self, id, data):      #SQL converter only needs id, not data
+        conn = connect('experiments.db')    #the name of the file to access can't be set manually right now 
+        cur = conn.cursor()
+        
+        names = {}
+        dependencies = {}
+
+        for row in cur.execute('SELECT layout_id, parameter FROM layouts WHERE run_id is ?', str(id)):
+            names[row[0]] = row[1]
+
+        for row in cur.execute('SELECT dependent, independent FROM dependencies'):
+            if row[0] in names.keys():
+                dependencies[names[row[0]]] = []
+            
+        for row in cur.execute('SELECT dependent, independent FROM dependencies'):     
+            if row[0] in names.keys():
+                dependencies[names[row[0]]].append(names[row[1]])
+
+        conn.close()
+
+        return dependencies
+
+    
+    def find_variables(self, data):
+        
+        all_variables = []
+        
+        for data_subset in data:
+            for variable in data_subset:
+                if variable['name'] not in all_variables:
+                    all_variables.append(variable['name'])
+                    
+        return all_variables
+
+    
+    def make_data_dictionary(self, data):
+        
+        data_dict = {}
+        
+        for data_subset in data:
+            for variable in data_subset:
+                
+                name = variable['name']
+        
+                if name in data_dict.keys():
+                    print(name)
+                    if not np.array_equal(variable['data'], data_dict[name]['data']):
+                        raise RuntimeError('Variables with identical names contain non-identical data arrays!')
+        
+                data_dict[name] = variable
+            
+        return data_dict
+            
+
+
+class Legacy_Converter(DataConverter):
+    
+    def find_data(self, id, samplefolder):
+        
+        str_id = '{0:03d}'.format(id)
+        
+        if samplefolder==None:
+            check_experiment_is_initialized()    
+            #check_experiment_is_initialized() doesn't do anything at the moment
+            
+            path = qc.DataSet.location_provider.fmt.format(counter=str_id)
+            data = qc.load_data(path)
+
+        else:
+            path = '{}{}{}'.format(samplefolder,sep,str_id)
+            data = qc.load_data(path)
+            
+        return data
+    
+    
+    def find_dependencies(self, id, data):    #legacy only needs data, not id
+        
+        dep_vars   = [key for key in data.arrays.keys() if "_set" not in key[-4:]]
+        indep_vars = [key for key in data.arrays.keys() if "_set" in key[-4:]]
+        
+        dependencies = {}
+        
+        for variable in dep_vars:
+            dependencies[variable] = indep_vars
+            
+        return dependencies
+    
+    
+    def find_variables(self, data):
+        
+        all_variables = [variable for variable in data.arrays.keys()]
+        return all_variables
+    
+    
+    def resize_data(self, data1, data2):
+    
+        scan_size = len(data1[0])
+    
+        new_data2 = []
+    
+        for setvalue in data2:
+            array = np.array([setvalue for datapoint in range(scan_size)])
+            new_data2.append(array)
+        
+        return np.array(new_data2)
+
+    
+    def make_data_dictionary(self, data):
+        
+        data_dict = {}
+        
+        all_variables = self.find_variables(data)
+        
+        for variable in all_variables:
+            qc_data = getattr(data, variable)
+            name = getattr(qc_data, 'name', 'No Name')
+            label = getattr(qc_data, 'label', '')
+            unit = getattr(qc_data, 'unit', '')
+            np_data = qc_data.ndarray
+            
+            data_dict[variable] = {'name': name,
+                                  'label': label,
+                                   'var_name': variable,
+                                  'unit': unit,
+                                  'data': np_data}
+            
+            
+        '''checks if the data set used set points - if so, it reformats the data
+        arrays so that it looks the same as it would look if it were imported
+        from SQL. All data is stored as arrays of the same length, rather than
+        set_points being stored as single numbers
+        (i.e. for setpoints = [1, 2, 3] , data = [[a, b, c], [d, e, f], [g, h, i]] 
+        becomes setpoints = [[1, 1, 1], [2, 2, 2], [3, 3, 3]], data = data)'''
+        dependencies = self.find_dependencies(id, data)
+        
+        dep_vars = [key for key in dependencies.keys()]
+        indep_vars = dependencies[dep_vars[0]]
+        
+        if len(indep_vars) == 2:
+            data1 = data_dict[dep_vars[0]]['data']    
+            data2 = data_dict[indep_vars[0]]['data']  
+            data3 = data_dict[indep_vars[1]]['data']
+            
+            if type(data2[0]) != np.ndarray:
+                data_dict[indep_vars[0]]['data'] = self.resize_data(data1, data2)
+            if type(data3[0]) != np.ndarray:
+                data_dict[indep_vars[1]]['data'] = self.resize_data(data1, data3)
+          
+        
+        
+            '''reformatting nested arrays as a single 1d array, i.e.
+            setpoints = [[1, 1, 1], [2, 2, 2], [3, 3, 3]], data = [[a, b, c], [d, e, f], [g, h, i]]
+            becomes setpoints [1, 1, 1, 2, 2, 2, 3, 3, 3], data = [a, b, c, d, e, f, g, h, i]'''
+            
+            for variable in all_variables:
+                data1 = data_dict[variable]['data']
+                
+                new_data = []
+                for array in data1:
+                    for datapoint in array:
+                        new_data.append(datapoint)
+                    
+                data_dict[variable]['data'] = np.array(new_data)
+                
+                
+        return data_dict
+       
+        
---
+++ qdev_wrappers/fitting/Fitter.py
@@ -0,0 +1,187 @@
+import qcodes as qc
+import numpy as np
+
+from Fitclasses import T1, T2
+from Converter import SQL_Converter, Legacy_Converter
+from scipy.optimize import curve_fit
+
+
+
+
+
+
+
+class Fitter():
+    
+    def do_1d(self, xdata, ydata, fitclass, p0,**kwargs):
+        
+        if (p0==None and hasattr(fitclass,'guess')):
+            p0 = getattr(fitclass,'guess')(xdata,ydata)      
+            popt, pcov = curve_fit(fitclass.fun, xdata, ydata, p0=p0, **kwargs)
+            return popt, pcov     
+        elif p0!=None:
+            popt, pcov = curve_fit(fitclass.fun, xdata, ydata, p0=p0, **kwargs)
+            return popt, pcov 
+        else:
+            return "Could not find guess parameters for fit."
+        
+
+    def fit(self, data, fitclass, x=None, y=None, z=None, default_dependencies=False, dataname = None, cut='vertical', p0=None,**kwargs):
+        
+        if type(fitclass) == type:  
+            #Maybe I'm just an idiot, and this isn't necessary for the world-at-large, but 
+            #I spent about 45 minutes trying to figure out what I broke before I realized 
+            #that I just forgot the parentheses after the fitclass. So this is here for now.
+            raise RuntimeError('It looks like there is something wrong with your fitclass(). Possibly you forgot the parentheses?')
+        
+        if (x==None or y==None) and default_dependencies==False:
+            raise RuntimeError('Please either specify data for x, y (and optionally z) or set default_dependencies = True')
+            
+        if default_dependencies == True:
+            dep_var = [key for key in data['dependencies'].keys()]
+            indep_vars = [value for value in data['dependencies'].values()]
+        
+            if dataname==None:
+                if len(dep_var) != 1:
+                    raise RuntimeError('Dataset has more than one dataname. Choose dataname to be fitted from: {}.'.format(', '.join(dep_var)))
+            
+                indep_vars = indep_vars[0]
+        
+            else:
+                if dataname not in dep_var:
+                    raise RuntimeError('Dataname not in dataset. Input dataname was: \'{}\' while dataname(s) in dataset are: {}.'.format(dataname,', '.join(dep_var)))
+            
+                indep_vars = indep_vars[dep_var.index(dataname)]
+                dep_var = [dataname]
+            
+                if len(indep_vars) > 2:
+                    print('independent variables: {}'.format(indep_vars))
+                    raise RuntimeError('That dataset seems to contain {} independent variables, which is {} too many.'.format(len(indep_vars), len(indep_vars)-2))
+
+        
+            if len(indep_vars) == 1:
+                dimensions = 1
+                x_dict = data[indep_vars[0]]
+                y_dict = data[dep_var[0]] 
+        
+            if len(indep_vars) == 2:
+                dimensions = 2
+                x_dict = data[indep_vars[0]]
+                y_dict = data[indep_vars[1]]
+                z_dict = data[dep_var[0]]
+            
+            
+        elif default_dependencies == False:
+            
+            for dataname in [x, y, z]:
+                if dataname not in data['variables']:
+                    raise RuntimeError('The specified variable "{}" is not found in the variables for this data dictionary. Variables are {}'.format(dataname, data['variables']))
+                    
+            x_dict = data[x]
+            y_dict = data[y]
+            dimensions = 1
+            
+            if z != None:
+                z_dict = data[z]
+                dimensions = 2
+    
+    
+        #Do fit for 1D data
+        if dimensions == 1:
+            
+            xdata = x_dict['data']     
+            ydata = y_dict['data'] 
+            
+            fit1d = {}
+            fit1d['parameters'] = {}
+                                                       
+            popt, pcov = self.do_1d(xdata, ydata, fitclass, p0,**kwargs)
+                
+            for parameter in fitclass.p_labels:   #parameters currently missing units, use fitclass.p_units
+                fit1d['parameters'][parameter] = {'value': popt[fitclass.p_labels.index(parameter)]}
+                fit1d['parameters'][parameter]['cov'] = pcov[fitclass.p_labels.index(parameter)]
+            
+            fit1d['estimator'] = {'method': 'Least squared fit',
+                                  'type': fitclass.name, 
+                                  'function': 'save all text from fitter function and class function here?',
+                                  'more?' : 'perhaps'} 
+   
+            fit1d['inferred_from'] = {'xdata': x_dict['name'], 
+                                            'ydata': y_dict['name'], 
+                                            'dataset': data['data_id'],
+                                            'dependencies': data['dependencies']} #missing sample name
+                    
+            
+            return fit1d
+        
+        
+        #Do fit for 2D data   
+        if dimensions == 2:
+            
+            xdata = x_dict['data']    
+            ydata = y_dict['data'] 
+            zdata = z_dict['data'] 
+            
+            fits2d = {}
+            
+            if cut == 'horizontal':
+                setarray = ydata
+                xarray = xdata
+                
+            if cut == 'vertical':
+                setarray = xdata
+                xarray = ydata
+            
+            setpoints = np.unique(setarray)
+            yarray = zdata
+            
+            #reformats as array of set points, with a y-array and z-array corresponding to each set point
+            
+            xdata_lst = []
+            ydata_lst = []
+            
+            for set_point in setpoints:
+                x_dat = []
+                y_dat = []         
+                for setpoint, x, y in zip(setarray, xarray, yarray): 
+                    if setpoint == set_point:
+                        x_dat.append(x)
+                        y_dat.append(y)
+                xdata_lst.append(np.array(x_dat))
+                ydata_lst.append(np.array(y_dat))
+                                  
+            
+            xdata = np.array(xdata_lst)
+            ydata = np.array(ydata_lst)  
+            
+            
+            #fitting as a sequence of 1D plots for different set_values
+            
+            for set_value, xdata_1d, ydata_1d in zip(setpoints, xdata, ydata):
+                
+                popt, pcov = self.do_1d(xdata_1d, ydata_1d, fitclass, p0,**kwargs)
+                
+                fits2d[set_value] = {}
+                fits2d[set_value]['parameters'] = {}
+                
+                for parameter in fitclass.p_labels:            #parameters currently missing units, use fitclass.p_units
+                    fits2d[set_value]['parameters'][parameter] = {'value': popt[fitclass.p_labels.index(parameter)]}
+                    fits2d[set_value]['parameters'][parameter]['cov'] = pcov[fitclass.p_labels.index(parameter)]
+            
+
+            fits2d['estimator'] = {'method': 'Least squared fit',
+                                  'type': fitclass.name, 
+                                  'function': 'save all text from fitter function and class function here?',
+                                  'more?' : 'perhaps'}
+            
+            
+            #does this needs to be moved so that it specifies which cut the individual sets of parameters are inferred from? 
+            fits2d['inferred_from'] = {'xdata': x_dict['name'],  
+                                            'ydata': y_dict['name'], 
+                                            'zdata': z_dict['name'], 
+                                            'dataset': data['data_id'],
+                                            'dependencies': data['dependencies']} #missing sample name
+                    
+            return fits2d            
+        
+
