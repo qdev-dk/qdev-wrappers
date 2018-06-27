c683818e8017448f5fe4aab6c22d6fc02d76740e
ThorvaldLarsen <thorvaldw@hotmail.com>
2018-06-27 18:15:45 +0200

    Fix frequency limit for SR860

--- qdev_wrappers/customised_instruments/SR86x_ext.py
+++ qdev_wrappers/customised_instruments/SR86x_ext.py
@@ -39,7 +39,7 @@ class SR860_ext(SR86x_ext):
     The SR860 instrument is almost equal to the SR865, except for the max frequency
     """
     def __init__(self, name: str, address: str, reset: bool=False, **kwargs: str) ->None:
-        super().__init__(name, address, max_frequency=5E3, reset=reset, **kwargs)
+        super().__init__(name, address, max_frequency=500E3, reset=reset, **kwargs)
 
 
 """
