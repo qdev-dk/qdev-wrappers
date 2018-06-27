fc361fa87a8662689776214fb06c9a67067a27de
ThorvaldLarsen <thorvaldw@hotmail.com>
2018-06-27 18:09:28 +0200

    remove comment

--- qdev_wrappers/customised_instruments/SR86x_ext.py
+++ qdev_wrappers/customised_instruments/SR86x_ext.py
@@ -1,9 +1,6 @@
 from qcodes.instrument_drivers.stanford_research.SR86x import SR86x
 
 class SR86x_ext(SR86x):
-    """
-    The SR860 instrument is almost equal to the SR865, except for the max frequency
-    """
     def __init__(self, name: str, address: str, **kwargs: str) ->None:
         super().__init__(name, address, **kwargs)
 
