3984328fe08a9b1c4a0d992ad38567fe309b75a5
Dominik Vogel <a-dovoge@microsoft.com>
2018-07-13 16:25:18 +0200

    Save metadata and add loading of sequences

--- qdev_wrappers/customised_instruments/parametric_sequencer.py
+++ qdev_wrappers/customised_instruments/parametric_sequencer.py
@@ -1,6 +1,6 @@
 import logging
 
-from typing import Dict
+from typing import Dict, Union
 from broadbean.types import ContextDict, ForgedSequenceType
 
 from copy import copy
@@ -45,30 +45,66 @@ class ParametricSequencer(Instrument):
                  initial_element: Element=None) -> None:
         super().__init__(name)
         self.awg = awg
+
         self.template_element = template_element
         self.initial_element = initial_element
-        self.context = context
-        self.setpoints = setpoints
+        self._context = {}
+        self._setpoints = {}
+        self._sequence_up_to_date = False
 
+        self.load_sequence(template_element=template_element,
+                           context=context,
+                           initial_element=initial_element,
+                           setpoints=setpoints,
+                           upload=True)
 
+    def load_sequence(self,
+                      template_element: Element,
+                      context: ContextDict,
+                      setpoints: Union[Dict, None]=None,
+                      initial_element: Union[Element, None]=None,
+                      upload=False):
+
+        self._sequence_up_to_date = False
+
+        self.template_element = template_element
+        self.initial_element = initial_element
+        # context:
+        for k in self._context:
+            self.parmeters[k].pop()
+        self._context = context
+        
         # TODO: implement context manager like in alazar driver
         # until then cheat to avoid multiple uploads:
-        self._do_update_on_set = False
+        self._do_upload_on_set = False
 
         # add non-step parameters
-        for name, value in self.context.items():
+        for name, value in self._context.items():
             # TODO: add units
             self.add_parameter(name=name,
                                get_cmd=None,
                                set_cmd=partial(self._set_context_parameter,
                                                name),
                                initial_value=value)
-        self._do_update_on_set = True
-        self._update_sequence()
-        self._upload_sequence()
+        self._do_upload_on_set = True
 
+        # add metadata, that gets added to the snapshot automatically
+        self.metadata['template_element'] = template_element
+        self.metadata['initial_element'] = template_element
+        self.metadata['context'] = context
+
+        if setpoints is not None:
+            self.load_setpoints(setpoints, upload=upload)
+        elif upload:
+            self._upload_sequence()
+        
+
+    def load_setpoints(self,
+                       setpoints: Dict,
+                       upload=True):
+        self._sequence_up_to_date = False
         # add step parameters
-        symbol = self.setpoints["symbol"]
+        symbol = setpoints["symbol"]
         parameter_name = f'step_{symbol}'
         self.add_parameter(name=parameter_name,
                            get_cmd=None,
@@ -76,13 +112,18 @@ class ParametricSequencer(Instrument):
         self._step_parameter = self.parameters[parameter_name]
         self._step_parameter(None)
 
-        # TODO: add setpoints and template to metadata
+        self._setpoints = setpoints
+        # add metadata, that gets added to the snapshot automatically
+        self.metadata['setpoints'] = setpoints
+
+        if upload:
+            self._upload_sequence()
 
 
     def _set_context_parameter(self, parameter, val):
-        self.context[parameter] = val
-        self._update_sequence()
-        self._upload_sequence()
+        self._context[parameter] = val
+        if self._do_upload_on_set:
+            self._upload_sequence()
 
     def _set_step(self, value):
         if value is None:
@@ -97,18 +138,24 @@ class ParametricSequencer(Instrument):
         self.awg.step(from_index, to_index)
 
     def _set_point_to_element_index(self, setpoint_value):
-        index = (np.abs(self.setpoints['values']
+        index = (np.abs(self._setpoints['values']
                         - setpoint_value)).argmin() + 1
         if self.initial_element is not None:
             index += 1
         return index
 
+    def _upload_sequence(self):
+        if not self._sequence_up_to_date:
+            self._update_sequence()
+        if self._do_upload_on_set:
+            self.awg.upload(self.sequence.forge(SR=self.awg.get_SR(),
+                                                context=self.sequence_context))
 
     def _update_sequence(self):
-        if self._do_update_on_set:
+        if self._do_upload_on_set:
             elements = []
-            for value in self.setpoints['values']:
-                kwarg = {self.setpoints['symbol']: value}
+            for value in self._setpoints['values']:
+                kwarg = {self._setpoints['symbol']: value}
                 new_element = in_context(self.template_element,
                                         **kwarg)
                 elements.append(new_element)
@@ -120,11 +167,9 @@ class ParametricSequencer(Instrument):
                 elements.insert(0, self.initial_element)
 
             self.sequence = Sequence(elements)
-            self.sequence_context = {k: v for k,v in self.context.items()
-                                    if k != self.setpoints['symbol']}
-
-    def _upload_sequence(self):
-        if self._do_update_on_set:
-            self.awg.upload(self.sequence.forge(SR=self.awg.get_SR(),
-                                                context=self.sequence_context))
-
+            self.sequence_context = {k: v for k,v in self._context.items()
+                                    if k != self._setpoints['symbol']}
+# data is added to the snapshot via the metadata attribute instead
+    # def snapshot_base(self, update: bool=False,
+    #                   params_to_skip_update: Sequence[str]=None):
+    #     # TODO: add setpoints and template to metadata
