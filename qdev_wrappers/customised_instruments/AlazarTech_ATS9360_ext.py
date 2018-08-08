a70e6c4da736d96ed95ae562c665cb184aeafa5f
nataliejpg <npearson@phys.ethz.ch>
2018-08-08 15:20:20 +0200

    change get and set cmds to take anything reasonable and return bools

--- qdev_wrappers/customised_instruments/AlazarTech_ATS9360_ext.py
+++ qdev_wrappers/customised_instruments/AlazarTech_ATS9360_ext.py
@@ -8,24 +8,30 @@ class AlazarTech_ATS9360_ext(AlazarTech_ATS9360):
         self.add_parameter(name='seq_mode',
                            get_cmd=self._get_seq_mod,
                            set_cmd=self._set_seq_mode,
-                           vals=vals.Enum('on', 'off'))
+                           vals=vals.Enum('on', 'off'),
+                           get_cmd=get_parser_on_off,
+                           set_cmd=set_parser_on_off)
 
     def _get_seq_mod(self):
         if (self.aux_io_mode() == 'AUX_IN_TRIGGER_ENABLE' and
                 self.aux_io_param() == 'TRIG_SLOPE_POSITIVE'):
-            return 'on'
+            return True
         elif (self.aux_io_mode() == 'AUX_IN_AUXILIARY' and
               self.aux_io_param() == 'NONE'):
-            return 'off'
+            return False
         else:
             raise ValueError('aux_io_mode: {}, aux_io_param: {} '
                              'do not correspond to seq_mode on or off')
 
     def _set_seq_mode(self, mode):
-        if mode == 'on':
+        if str(mode).upper() in ['1', 'ON', 'TRUE']:
             self.aux_io_mode('AUX_IN_TRIGGER_ENABLE')
             self.aux_io_param('TRIG_SLOPE_POSITIVE')
-        elif mode == 'off':
+        elif str(mode).upper() in ['0', 'OFF', 'FALSE']:
             self.aux_io_mode('AUX_IN_AUXILIARY')
             self.aux_io_param('NONE')
+        else:
+            raise ValueError(
+                'Unable to set seq_mode to {} '
+                'expected "ON"/1/True or "OFF"/0/False.'.format(mode))
         self.sync_settings_to_card()
