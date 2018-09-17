06a0c29236f632855f493f9bead87d71c9a934ab
nataliejpg <npearson@phys.ethz.ch>
2018-09-17 15:18:56 +0200

    saves file to same folder as pngs and has parameters as header

--- qdev_wrappers/export.py
+++ qdev_wrappers/export.py
@@ -1,28 +1,26 @@
 import numpy as np
 from qcodes.dataset.data_set import load_by_id
-from qcodes.dataset.experiment_container import (load_last_experiment,
-                                                 load_experiment)
+from qcodes.dataset.experiment_container import load_experiment
 from qcodes.config.config import Config
 import os
 
 
-def get_export_path(run_id):
+def get_export_path(run_id, exp_id):
     '''Get the full path to save the exported data at.'''
     extension = 'txt'
     db_path = Config()['core']['db_location']
+    experiment = load_experiment(exp_id)
     db_folder = os.path.dirname(db_path)
-    export_folder_name = 'export/OneDrive'
-    export_folder = os.path.join(db_folder, export_folder_name)
-    os.makedirs(export_folder, exist_ok=True)
+    sample_folder_name = '{}_{}'.format(experiment.sample_name, experiment.name)
+    sample_folder = os.path.join(db_folder, sample_folder_name)
+    os.makedirs(sample_folder, exist_ok=True)
     filename = '{}.{}'.format(run_id, extension)
-    plot_path = os.path.join(export_folder, filename)
+    plot_path = os.path.join(sample_folder, filename)
     return plot_path
-
 
 def export_by_id(run_id):
     '''Export CSV files with raw data of a measurement run.'''
     dataset = load_by_id(run_id)
-    # experiment = load_experiment(dataset.exp_id)
     data = []
     headers = []
     for parameter_name, specs in dataset.paramspecs.items():
@@ -31,6 +29,6 @@ def export_by_id(run_id):
         parameter_header = '{} ({})'.format(specs.label, specs.unit)
         headers.append(parameter_header)
     data = np.vstack(data).T
-    header = ', '.join(headers)
-    filename = get_export_path(run_id)
-    np.savetxt(filename, data)
+    header = ' '.join(headers)
+    filename = get_export_path(run_id, dataset.exp_id)
+    np.savetxt(filename, data, header=header)
