b51b79d9ac7fc4140a370d795df10c1ce0b71909
nataliejpg <npearson@phys.ethz.ch>
2018-10-31 15:34:10 +0100

    added some semblance of examples

--- examples/settings_instrument/settings_example.py
+++
@@ -1,3 +0,0 @@
-from qdev_wrappers.customised_instruments.settings_instrument import SettingsInstrument
-
-settings = SettingsInstrument('settings', 3, 'default_settings.yaml', file_to_save='specific_settings.yaml')
