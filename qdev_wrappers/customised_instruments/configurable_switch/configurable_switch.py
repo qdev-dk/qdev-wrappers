15d310ade2284c4a35ce0b8b49dfa4661bc69b05
nataliejpg <npearson@phys.ethz.ch>
2018-12-06 17:17:38 +0100

    refactor and add simulated version

--- qdev_wrappers/customised_instruments/configurable_switch/configurable_switch.py
+++ qdev_wrappers/customised_instruments/configurable_switch/configurable_switch.py
@@ -3,9 +3,8 @@ from qcodes import Instrument
 from qcodes.utils import validators as vals
 
 
-class ConfigurableSwitch(Instrument):
-    def __init__(self, name, switch_configuration, switch):
-        self._switch = switch
+class _ConfigurableSwitchBase(Instrument):
+    def __init__(self, name, switch_configuration):
         self._switch_configuration = switch_configuration
         super().__init__(name)
         self.add_parameter(
@@ -16,9 +15,23 @@ class ConfigurableSwitch(Instrument):
             setattr(name, partial(self._set_configuration, name))
 
     def _set_configuration(self, val):
+        raise NotImplementedError
+
+
+class ConfigurableSwitch(_ConfigurableSwitchBase):
+    def __init__(self, name, switch_configuration, switch):
+        self._switch = switch
+        super().__init__(name, switch_configuration)
+
+    def _set_configuration(self, val):
         settings = self._switch_configuration[val]
         for k, v in settings.items():
             try:
                 self._switch.parameters[k](v)
             except KeyError:
                 self._switch.submodules[k](v)
+
+
+class SimulatedConfigurableSwitch(Instrument):
+    def _set_configuration(self, val):
+        pass
