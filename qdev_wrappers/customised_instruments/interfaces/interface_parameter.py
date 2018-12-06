689f97aaa6129e968e8fce3706869e31164d91e0
nataliejpg <npearson@phys.ethz.ch>
2018-12-06 15:33:21 +0100

    change functions to have priority over source and add docstring

--- qdev_wrappers/customised_instruments/interfaces/interface_parameter.py
+++ qdev_wrappers/customised_instruments/interfaces/interface_parameter.py
@@ -1,27 +1,35 @@
 from qcodes.instrument.parameter import Parameter
 
-# TODO: doctrings
 
 class InterfaceParameter(Parameter):
     def __init__(self, name, source=None, get_fn=None,
                  set_fn=None, *args, **kwargs):
+    """
+    Parameter which by default behaves like ManualParameter but can
+    be easily configured to get/set a source parameter or run a
+    function when get/set is called. The functions if specified
+    have priority over the source. If a function is False
+    it means the parameter cannot be get/set.
+    """
         self._source = source
         self._set_fn = set_fn
         self._get_fn = get_fn
         super().__init__(name=name, *args, **kwargs)
 
     def get_raw(self):
-        if self._source is not None:
-            return self.source.get()
+        if self._get_fn is False:
+            raise RuntimeError(f'Parmeter {self.name} not gettable')
         elif self._get_fn is not None:
             return self._get_fn()
+        elif self._source is not None:
+            return self.source.get()
         else:
             return self._latest['value']
 
     def set_raw(self, val):
-        if self._source is not None:
-            self.source.set(val)
-        elif self._set_fn is False:
+        if self._set_fn is False:
             raise RuntimeError(f'Parmeter {self.name} not settable')
         elif self._set_fn is not None:
             self._set_fn(val)
+        elif self._source is not None:
+            self.source.set(val)
