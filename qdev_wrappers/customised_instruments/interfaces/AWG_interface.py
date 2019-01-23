02d3f53ad9e085b5dd986f5988b3e454086752ec
nataliejpg <npearson@phys.ethz.ch>
2019-01-23 15:59:46 +0100

    awg interface example to work, needs testing with real awg

--- qdev_wrappers/customised_instruments/interfaces/AWG_interface.py
+++ qdev_wrappers/customised_instruments/interfaces/AWG_interface.py
@@ -1,5 +1,5 @@
-from broadbean.types import ForgedSequenceType
-from broadbean.plotting import plotter
+from lomentum.types import ForgedSequenceType
+from lomentum.plotting import plotter
 from time import sleep
 import qcodes.utils.validators as vals
 from qcodes.instrument.base import Instrument
@@ -36,10 +36,10 @@ class _AWGInterface(Instrument):
                            unit='Hz',
                            parameter_class=DelegateParameter)
         self.add_parameter(name='sequence_mode',
-                           set_cmd=self._set_seq_mode,
+                           set_cmd=self.set_sequence_mode,
                            vals=vals.Enum('element', 'sequence'))
         self.add_parameter(name='repetition_mode',
-                           set_cmd=self._set_rep_mode,
+                           set_cmd=self.set_repetition_mode,
                            vals=vals.Enum('single', 'inf'))
         self.add_parameter('sleep_time',
                            label='Sleep time',
@@ -48,6 +48,8 @@ class _AWGInterface(Instrument):
                            set_cmd=None,
                            docstring="Time to sleep after "
                                      "setting repeated_element")
+        self.sequence_mode._save_val('sequence')
+        self.repetition_mode._save_val('inf')
 
     def upload(self, forged_sequence: ForgedSequenceType):
         """
@@ -100,7 +102,9 @@ class SimulatedAWGInterface(_AWGInterface):
         self.CHAN_NUM = chan_num
         super().__init__(name)
         self.to_default()
-        self.sequence_mode._save_val()
+        self.sequence_mode._save_val('sequence')
+        self.repetition_mode._save_val('inf')
+        self.sleep_time(0)
 
     def upload(self, forged_sequence: ForgedSequenceType):
         print(f'uploading')
@@ -109,15 +113,27 @@ class SimulatedAWGInterface(_AWGInterface):
         self.index = 0
         if self.sequence_mode() == 'element':
             self.set_element(0)
-        plotter(forged_sequence, SR=self.sample_rate())
+        else:
+            sleep(self.sleep_time())
+            plotter(forged_sequence, SR=self.sample_rate())
+            if self.repetition_mode() == 'inf':
+                self.run()
 
     def set_sequence_mode(self, seq_mode):
         print(f'setting sequence_mode to {seq_mode}')
         if seq_mode == 'element':
             self.set_element(self.index)
+        else:
+            sleep(self.sleep_time())
+            plotter(self.forged_sequence, SR=self.sample_rate())
+            if self.repetition_mode() == 'inf':
+                self.run()
 
     def set_repetition_mode(self, rep_mode):
         print(f'setting repetition_mode to {rep_mode}')
+        sleep(self.sleep_time())
+        if rep_mode == 'inf':
+            self.run()
 
     def set_element(self, index):
         print(f'setting element to {index}')
@@ -127,7 +143,10 @@ class SimulatedAWGInterface(_AWGInterface):
                 'longer than the last sequence index {self.last_index}')
         self.sequence_mode._save_val('element')
         self.index = index
-        plotter(forged_sequence[index], SR=self.sample_rate())
+        sleep(self.sleep_time())
+        plotter(self.forged_sequence[index], SR=self.sample_rate())
+        if self.repetition_mode() == 'inf':
+            self.run()
 
     def run(self):
         print('running')
@@ -138,6 +157,7 @@ class AWG5014Interface(_AWGInterface):
 
     def __init__(self, name, awg):
         self.awg = awg
+        self.awg.delete_all_waveforms_from_list()
         super().__init__(name)
         self.sample_rate.source = awg.clock_freq
         for ch in np.range(self.CHAN_NUM):
@@ -151,10 +171,17 @@ class AWG5014Interface(_AWGInterface):
         self.last_index = len(forged_sequence) - 1
         if self.sequence_mode() == 'element':
             self.set_element(0)
+        elif self.repetition_mode() == 'single':
+            last_index = self.last_index + 1
+            self.awg.set_sqel_goto_target_index(last_index, 0)
+            sleep(self.sleep_time())
+        else:
+            self.run()
 
     def set_sequence_mode(self, seq_mode):
         if seq_mode == 'element':
             self.set_element(self.index)
+            return
         elif seq_mode == 'sequence':
             rep_mode = self.repetition_mode()
             index = self.index + 1
@@ -166,6 +193,8 @@ class AWG5014Interface(_AWGInterface):
                 self.awg.set_sqel_loopcnt_to_inf(index, state=0)
                 self.awg.set_sqel_goto_target_index(last_index, 1)
         sleep(self.sleep_time())
+        if rep_mode == 'inf':
+            self.run()
 
     def set_repetition_mode(self, rep_mode):
         seq_mode = self.sequence_mode()
@@ -188,6 +217,8 @@ class AWG5014Interface(_AWGInterface):
                 # at the end of the sequence start again
                 self.awg.set_sqel_goto_target_index(last_index, 1)
         sleep(self.sleep_time())
+        if rep_mode == 'inf':
+            self.run()
 
     def set_element(self, index):
         if index > self.last_index:
@@ -227,4 +258,6 @@ class AWG5014Interface(_AWGInterface):
             self.awg.set_sqel_loopcnt_to_inf(new_index, state=1)
         self.awg.sequence_pos(new_index)
         self.index = index
-        sleep(self.sleep_time()
+        sleep(self.sleep_time())
+        if rep_mode == 'inf':
+            self.run()
